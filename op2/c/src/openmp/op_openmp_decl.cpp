21bedb1025940503a347151a5365169da7c180b5
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-10 14:46:38 +0100

    modified op_decl_dat

--- op2/c/src/openmp/op_openmp_decl.cpp
+++ op2/c/src/openmp/op_openmp_decl.cpp
@@ -17,8 +17,9 @@ void op_init(int argc, char **argv, int diags){
   op_init_core(argc, argv, diags);
 }
 
-op_dat op_decl_dat_char(op_set set, int dim, char const *type,
-                        int size, char *data, char const *name){
+op_dat op_decl_dat ( op_set set, int dim, char const *type,
+                        int size, char *data, char const *name )
+{
   return op_decl_dat_core(set, dim, type, size, data, name);
 }
 
