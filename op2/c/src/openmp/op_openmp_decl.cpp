17b069699e1b964676d14165b337214c534dfb72
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-10 14:30:26 +0100

    added wrappers to openmp declarations

--- op2/c/src/openmp/op_openmp_decl.cpp
+++ op2/c/src/openmp/op_openmp_decl.cpp
@@ -36,3 +36,30 @@ void op_exit ()
 
   op_exit_core ();
 }
+
+//
+// Wrappers of core lib
+//
+
+op_set op_decl_set ( int size, char const * name )
+{
+  return op_decl_set_core ( size, name );
+}
+
+
+op_map op_decl_map ( op_set from, op_set to, int dim, int * imap, char const * name )
+{
+  return op_decl_map_core ( from, to, dim, imap, name );
+}
+
+
+op_arg op_arg_dat ( op_dat dat, int idx, op_map map, int dim, char const * type, op_access acc )
+{
+  return op_arg_dat_core ( dat, idx, map, dim, type, acc );
+}
+
+
+op_arg op_arg_gbl ( char * data, int dim, const char * type, op_access acc )
+{
+  return op_arg_gbl ( data, dim, type, acc );
+}
