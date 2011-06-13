ef1ba9156c94b37fdaeb41afd353565769dd90c7
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-13 12:23:23 +0100

    added fortran file (to be compiled and tested); removed extern C from core library

---
+++ op2/fortran/src/backend/op2_C_reference.c
@@ -0,0 +1,185 @@
+
+/*
+ * This file implements the reference (sequential) version of op_par_loop calls
+ * to be used in Fortran
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <op_lib_core.h>
+#include <op_seq.h>
+
+
+void arg_set ( int displacement, /* set element */
+			   op_dat arg,
+			   int itemSel, /* map field to be used */
+			   op_map mapIn,
+			   char ** p_arg )
+{
+  int n2;
+
+  if ( mapIn->dim == -1 ) /* global variable, no mapping at all */
+  {
+	n2 = 0;
+  }
+	
+  if ( mapIn->dim == 0 ) /* identity mapping */
+  {
+    n2 = displacement;
+  }
+  if ( mapIn->dim > 0 ) /* standard pointers */
+  {
+    n2 = mapIn->map[itemSel + displacement * mapIn->dim];
+  }
+	
+  *p_arg = (char *) ( arg->data + n2 * arg->size );
+}                                                                                        
+
+
+
+void op_par_loop_2 ( void (*subroutineName)(char *, char *), op_set set,
+					 op_dat dat0, int itemSel0, op_map map0, op_access access0,
+					 op_dat dat1, int itemSel1, op_map map1, op_access access1
+				 )
+{
+	int i;
+
+	for ( i = 0; i < set->size; i++ ) {
+		char * ptr0, * ptr1;
+		
+		arg_set ( i, dat0, itemSel0, map0, &ptr0 );
+		arg_set ( i, dat1, itemSel1, map1, &ptr1 );
+		
+		(*subroutineName) ( ptr0, ptr1 );
+	}
+}
+
+void op_par_loop_5 ( void (*subroutineName)(char *, char *, char *, char *, char *), op_set set,
+					op_dat dat0, int itemSel0, op_map map0, op_access access0,
+					op_dat dat1, int itemSel1, op_map map1, op_access access1,
+					op_dat dat2, int itemSel2, op_map map2, op_access access2,
+					op_dat dat3, int itemSel3, op_map map3, op_access access3,
+					op_dat dat4, int itemSel4, op_map map4, op_access access4
+				 )
+{
+	int i;
+	
+	for ( i = 0; i < set->size; i++ ) {
+		
+		char * ptr0, * ptr1, * ptr2, * ptr3, * ptr4;
+		
+		arg_set ( i, dat0, itemSel0, map0, &ptr0 );
+		arg_set ( i, dat1, itemSel1, map1, &ptr1 );
+		arg_set ( i, dat2, itemSel2, map2, &ptr2 );
+		arg_set ( i, dat3, itemSel3, map3, &ptr3 );
+		arg_set ( i, dat4, itemSel4, map4, &ptr4 );
+		
+		
+		(*subroutineName) ( ptr0, ptr1, ptr2, ptr3, ptr4 );
+	}	
+}
+
+void op_par_loop_6 ( void (*subroutineName)(char *, char *, char *, char *, char *, char *), op_set set,
+					op_dat dat0, int itemSel0, op_map map0, op_access access0,
+					op_dat dat1, int itemSel1, op_map map1, op_access access1,
+					op_dat dat2, int itemSel2, op_map map2, op_access access2,
+					op_dat dat3, int itemSel3, op_map map3, op_access access3,
+					op_dat dat4, int itemSel4, op_map map4, op_access access4,
+					op_dat dat5, int itemSel5, op_map map5, op_access access5
+					)
+{
+	int i;
+		
+	for ( i = 0; i < set->size; i++ ) {
+
+		char * ptr0, * ptr1, * ptr2, * ptr3, * ptr4, * ptr5;
+		
+		arg_set ( i, dat0, itemSel0, map0, &ptr0 );
+		arg_set ( i, dat1, itemSel1, map1, &ptr1 );
+		arg_set ( i, dat2, itemSel2, map2, &ptr2 );
+		arg_set ( i, dat3, itemSel3, map3, &ptr3 );
+		arg_set ( i, dat4, itemSel4, map4, &ptr4 );
+		arg_set ( i, dat5, itemSel5, map5, &ptr5 );
+
+		
+		(*subroutineName) ( ptr0, ptr1, ptr2, ptr3, ptr4, ptr5 );
+
+	}	
+}
+
+void op_par_loop_8 ( void (*subroutineName)(char *, char *, char *, char *, char *, char *, char *, char *), op_set set,
+					op_dat dat0, int itemSel0, op_map map0, op_access access0,
+					op_dat dat1, int itemSel1, op_map map1, op_access access1,
+					op_dat dat2, int itemSel2, op_map map2, op_access access2,
+					op_dat dat3, int itemSel3, op_map map3, op_access access3,
+					op_dat dat4, int itemSel4, op_map map4, op_access access4,
+					op_dat dat5, int itemSel5, op_map map5, op_access access5,
+					op_dat dat6, int itemSel6, op_map map6, op_access access6,
+					op_dat dat7, int itemSel7, op_map map7, op_access access7
+					)
+{
+	int i;
+	
+	for ( i = 0; i < set->size; i++ ) {
+		
+		char * ptr0, * ptr1, * ptr2, * ptr3, * ptr4, * ptr5, * ptr6, * ptr7;
+		
+		arg_set ( i, dat0, itemSel0, map0, &ptr0 );
+		arg_set ( i, dat1, itemSel1, map1, &ptr1 );
+		arg_set ( i, dat2, itemSel2, map2, &ptr2 );
+		arg_set ( i, dat3, itemSel3, map3, &ptr3 );
+		arg_set ( i, dat4, itemSel4, map4, &ptr4 );
+		arg_set ( i, dat5, itemSel5, map5, &ptr5 );
+		arg_set ( i, dat6, itemSel6, map6, &ptr6 );
+		arg_set ( i, dat7, itemSel7, map7, &ptr7 );
+		
+		
+		(*subroutineName) ( ptr0, ptr1, ptr2, ptr3, ptr4, ptr5, ptr6, ptr7 );
+		
+	}	
+}
+
+void op_par_loop_12 ( void (*subroutineName)(char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *), op_set * set,
+					  op_dat * dat0, int itemSel0, op_map * map0, op_access access0,
+					  op_dat * dat1, int itemSel1, op_map * map1, op_access access1,
+					  op_dat * dat2, int itemSel2, op_map * map2, op_access access2,
+					  op_dat * dat3, int itemSel3, op_map * map3, op_access access3,
+					  op_dat * dat4, int itemSel4, op_map * map4, op_access access4,
+					  op_dat * dat5, int itemSel5, op_map * map5, op_access access5,
+					  op_dat * dat6, int itemSel6, op_map * map6, op_access access6,
+					  op_dat * dat7, int itemSel7, op_map * map7, op_access access7,
+					  op_dat * dat8, int itemSel8, op_map * map8, op_access access8,
+					  op_dat * dat9, int itemSel9, op_map * map9, op_access access9,
+					  op_dat * dat10, int itemSel10, op_map * map10, op_access access10,
+					  op_dat * dat11, int itemSel11, op_map * map11, op_access access11
+					)
+{
+	int i;
+
+	for ( i = 0; i < set->size; i++ ) {
+
+		char * ptr0, * ptr1, * ptr2, * ptr3, * ptr4, * ptr5, * ptr6, * ptr7, * ptr8, * ptr9, * ptr10, * ptr11;
+
+		arg_set ( i, dat0, itemSel0, map0, &ptr0 );
+		arg_set ( i, dat1, itemSel1, map1, &ptr1 );
+		arg_set ( i, dat2, itemSel2, map2, &ptr2 );
+		arg_set ( i, dat3, itemSel3, map3, &ptr3 );
+		arg_set ( i, dat4, itemSel4, map4, &ptr4 );
+		arg_set ( i, dat5, itemSel5, map5, &ptr5 );
+		arg_set ( i, dat6, itemSel6, map6, &ptr6 );
+		arg_set ( i, dat7, itemSel7, map7, &ptr7 );
+    arg_set ( i, dat8, itemSel8, map8, &ptr8 );
+    arg_set ( i, dat9, itemSel9, map9, &ptr9 );
+    arg_set ( i, dat10, itemSel10, map10, &ptr10 );
+    arg_set ( i, dat11, itemSel11, map11, &ptr11 );
+
+
+		(*subroutineName) ( ptr0, ptr1, ptr2, ptr3, ptr4, ptr5, ptr6, ptr7, ptr8, ptr9, ptr10, ptr11 );
+
+	}
+}
+
---
+++ op2/fortran/src/backend/op2_for_rt_support.f90
@@ -0,0 +1,37 @@
+! This module defines the interoperable data structures of the OP2 RT support
+! (i.e. plan struct) and defines the interface for the C plan function
+
+
+module OP2_Fortran_RT_Support
+
+	! not needed for now
+	! use OP2_Fortran_Declarations
+
+	interface
+
+		! C wrapper to plan function for Fortran (cPlan function)
+		type(c_ptr) function cplan ( name, setId, argsNumber, args, idxs, maps, accs, indsNumber, inds ) &
+							& BIND(C,name='FortranPlanCaller')
+
+			use, intrinsic :: ISO_C_BINDING
+
+			import :: c_int, c_ptr
+
+			character(kind=c_char) :: name(*) ! name of kernel
+			integer(kind=c_int), value :: setId ! position in OP_set_list of the related set
+			integer(kind=c_int), value :: argsNumber ! number of op_dat arguments to op_par_loop
+			integer(kind=c_int) :: args(*) ! positions in OP_dat_list of arguments to op_par_loop
+			integer(kind=c_int) :: idxs(*) ! array of indexes to maps
+			integer(kind=c_int) :: maps(*) ! positions in OP_map_list of arguments to op_par_loop
+			integer(kind=c_int) :: accs(*) ! access flags to arguments
+			integer(kind=c_int), value :: indsNumber ! number of arguments accessed indirectly via a map
+
+			! indexes for indirectly accessed arguments (same indrectly accessed argument = same index)
+			integer(kind=c_int), dimension(*) :: inds
+
+		end function cplan
+
+	end interface
+
+
+end module OP2_Fortran_RT_Support
