22771b8f9ae4c97c6505c82bb895f5d54aeb11ce
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-17 16:12:35 +0100

    added file for templated C++ cuda reduction routine

---
+++ op2/c/include/op_cuda_reduction.h
@@ -0,0 +1,82 @@
+#ifndef __OP_CUDA_REDUCTION_H
+#define __OP_CUDA_REDUCTION_H
+
+//
+// This file provides an optimised implementation for OP2 global variable reductions
+// It is separated from the op_cuda_rt_support.h file because the reduction code
+// is based on C++ templates, while the other file only includes C routines
+//
+
+#include <cuda.h>
+
+
+//
+// reduction routine for arbitrary datatypes
+//
+
+template < op_access reduction, class T >
+__inline__ __device__ void op_reduction(volatile T *dat_g, T dat_l)
+{
+  int tid = threadIdx.x;
+  int d   = blockDim.x>>1;
+  extern __shared__ T temp[];
+
+  __syncthreads();  // important to finish all previous activity
+
+  temp[tid] = dat_l;
+
+  for (; d>warpSize; d>>=1) {
+    __syncthreads();
+    if (tid<d) {
+      switch (reduction) {
+        case OP_INC:
+          temp[tid] = temp[tid] + temp[tid+d];
+          break;
+        case OP_MIN:
+          if(temp[tid+d]<temp[tid]) temp[tid] = temp[tid+d];
+          break;
+        case OP_MAX:
+          if(temp[tid+d]>temp[tid]) temp[tid] = temp[tid+d];
+          break;
+      }
+    }
+  }
+
+  __syncthreads();
+
+  volatile T *vtemp = temp;   // see Fermi compatibility guide
+
+  if (tid<warpSize) {
+    for (; d>0; d>>=1) {
+      if (tid<d) {
+        switch (reduction) {
+          case OP_INC:
+            vtemp[tid] = vtemp[tid] + vtemp[tid+d];
+            break;
+          case OP_MIN:
+            if(vtemp[tid+d]<vtemp[tid]) vtemp[tid] = vtemp[tid+d];
+            break;
+          case OP_MAX:
+            if(vtemp[tid+d]>vtemp[tid]) vtemp[tid] = vtemp[tid+d];
+            break;
+        }
+      }
+    }
+  }
+
+  if (tid==0) {
+    switch (reduction) {
+      case OP_INC:
+        *dat_g = *dat_g + vtemp[0];
+        break;
+      case OP_MIN:
+        if(temp[0]<*dat_g) *dat_g = vtemp[0];
+        break;
+      case OP_MAX:
+        if(temp[0]>*dat_g) *dat_g = vtemp[0];
+        break;
+    }
+  }
+}
+
+#endif
--- op2/c/include/op_cuda_rt_support.h
+++ op2/c/include/op_cuda_rt_support.h
@@ -40,7 +40,6 @@
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 #include <device_functions.h>
-#include <driver_types.h>
 
 #include "op_lib_core.h"
 #include "op_rt_support.h"
@@ -106,81 +105,5 @@ void mvReductArraysToDevice ( int reduct_bytes );
 void mvReductArraysToHost ( int reduct_bytes );
 
 
-//
-// reduction routine for arbitrary datatypes
-//
-
-//template < op_access reduction, class T >
-//__inline__ __device__ void op_reduction ( volatile T *dat_g, T dat_l );
-
-//
-// reduction routine for arbitrary datatypes
-//
-
-template < op_access reduction, class T >
-  __inline__ __device__ void op_reduction(volatile T *dat_g, T dat_l)
-{
-  int tid = threadIdx.x;
-  int d   = blockDim.x>>1;
-  extern __shared__ T temp[];
-
-  __syncthreads();  // important to finish all previous activity
-
-  temp[tid] = dat_l;
-
-  for (; d>warpSize; d>>=1) {
-    __syncthreads();
-    if (tid<d) {
-      switch (reduction) {
-      case OP_INC:
-        temp[tid] = temp[tid] + temp[tid+d];
-        break;
-      case OP_MIN:
-        if(temp[tid+d]<temp[tid]) temp[tid] = temp[tid+d];
-        break;
-      case OP_MAX:
-        if(temp[tid+d]>temp[tid]) temp[tid] = temp[tid+d];
-        break;
-      }
-    }
-  }
-
-  __syncthreads();
-
-  volatile T *vtemp = temp;   // see Fermi compatibility guide
-
-  if (tid<warpSize) {
-    for (; d>0; d>>=1) {
-      if (tid<d) {
-        switch (reduction) {
-        case OP_INC:
-          vtemp[tid] = vtemp[tid] + vtemp[tid+d];
-          break;
-        case OP_MIN:
-          if(vtemp[tid+d]<vtemp[tid]) vtemp[tid] = vtemp[tid+d];
-          break;
-        case OP_MAX:
-          if(vtemp[tid+d]>vtemp[tid]) vtemp[tid] = vtemp[tid+d];
-          break;
-        }
-      }
-    }
-  }
-
-  if (tid==0) {
-    switch (reduction) {
-    case OP_INC:
-      *dat_g = *dat_g + vtemp[0];
-      break;
-    case OP_MIN:
-      if(temp[0]<*dat_g) *dat_g = vtemp[0];
-      break;
-    case OP_MAX:
-      if(temp[0]>*dat_g) *dat_g = vtemp[0];
-      break;
-    }
-  }
-
-}
 
 #endif
