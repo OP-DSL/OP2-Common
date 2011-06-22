57000dc8a770ea69295a783a1c649d98be7e29e8
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-22 16:01:05 +0100

    removed print out from op_decl_dat for cuda and added setCacheconfig

--- op2/c/src/cuda/op_cuda_decl.c
+++ op2/c/src/cuda/op_cuda_decl.c
@@ -30,8 +30,7 @@ void op_init(int argc, char **argv, int diags){
 
   cutilDeviceInit(argc, argv);
 
-  
-  //  cutilSafeCall(cudaThreadSetCacheConfig(cudaFuncCachePreferShared));
+  cutilSafeCall(cudaThreadSetCacheConfig(cudaFuncCachePreferShared));
   printf("\n 16/48 L1/shared \n");
   
 }
@@ -41,11 +40,9 @@ op_dat op_decl_dat ( op_set set, int dim, char const *type,
 {
   op_dat dat = op_decl_dat_core ( set, dim, type, size, data, name );
 
-  printf ( "Copy from host to device of %d\n", dat->size * set->size );
-
-        op_cpHostToDevice((void **)&(dat->data_d),
-                    (void **)&(dat->data),
-                             dat->size*set->size);
+  op_cpHostToDevice ( (void **) &(dat->data_d),
+                      (void **) &(dat->data),
+                       dat->size*set->size );
   
   return dat;
 }
