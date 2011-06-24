57bce2cf326fa2df78f156e63fd5da8fbe85da61
Dr. Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-24 17:23:16 +0100

    modified fortran-c rt wrappers

--- op2/fortran/src/backend/op2_for_rt_wrappers.c
+++ op2/fortran/src/backend/op2_for_rt_wrappers.c
@@ -2,7 +2,7 @@
 #include <op_lib_core.h>
 #include <op_rt_support.h>
 #include <op_lib_c.h>
-#include <op_cuda_rt_support.h>
+//#include <op_cuda_rt_support.h>
 
 /* These numbers must corresponds to those declared in op2_for_rt_support.f90 */
 #define F_OP_ARG_DAT 0
@@ -39,97 +39,84 @@ op_access getAccFromIntCode ( int accCode )
   }
 }
 
-/*
- * Wrapper for Fortran to plan function
- */
 
-op_plan * FortranPlanCaller ( char name[],
-							  int setId,
-							  int argsNumber,
-							  int args[],
-							  int idxs[],
-							  int maps[],
-							  int accs[],
-							  int indsNumber,
-							  int inds[],
-                int argsType[],
-                int partitionSize
-							)
+op_arg * generatePlanInputData ( char name[],
+                                 int setId,
+                                 int argsNumber,
+                                 int args[],
+                                 int idxs[],
+                                 int maps[],
+                                 int accs[],
+                                 int indsNumber,
+                                 int inds[],
+                                 int argsType[]               
+                               )
 {
+  int i, generatedPlanIndex = ERR_INDEX;
 
-	int i, generatedPlanIndex = ERR_INDEX;
+  op_dat_core planDatArgs[argsNumber];
+  op_map_core planMaps[argsNumber];
+  int planDims[argsNumber];
+  char * planTypes[argsNumber];
+  op_access planAccs[argsNumber];
+  op_arg * planArguments;
 
-	op_plan * generatedPlan = NULL;
+  planArguments = calloc ( argsNumber, sizeof ( op_arg ) );
 
-	op_dat_core planDatArgs[argsNumber];
-	op_map_core planMaps[argsNumber];
-	int planDims[argsNumber];
-	char * planTypes[argsNumber];
-	op_access planAccs[argsNumber];
-	op_arg * planArguments;
-	op_set_core * iterationSet =  OP_set_list[setId];
 
-	planArguments = calloc ( argsNumber, sizeof ( op_arg ) );
+  /* build planDatArgs variable by accessing OP_dat_list with indexes(=positions) in args */
+  for ( i = 0; i < argsNumber; i++ )
+  {
+    op_dat_core * tmp = OP_dat_list[args[i]];
+    planDatArgs[i] = *tmp;
+  }
 
-	if ( iterationSet == NULL )
-	{
-		printf ( "bad set index\n" );
-		exit ( -1 );
-	}
+  /* build planMaps variables by accessing OP_map_list with indexes(=positions) in args */
+  for ( i = 0; i < argsNumber; i++ )
+  {
+    op_map_core * tmp;
+    int j;
 
-	/* build planDatArgs variable by accessing OP_dat_list with indexes(=positions) in args */
-	for ( i = 0; i < argsNumber; i++ )
-	{
-		op_dat_core * tmp = OP_dat_list[args[i]];
-		planDatArgs[i] = *tmp;
-	}
+    if ( inds[i] >= 0 ) /* another magic number !!! */
+    {
+      int iter;
+      tmp = OP_map_list[maps[i]];
+      planMaps[i] = *tmp;
+    }
+    else
+    {
+      /* build false map with index = -1 ... */
+      op_map_core * falseMap = (op_map_core *) calloc ( 1, sizeof ( op_map_core ) ); //OP_ID;
+      falseMap->index = -1;
+      planMaps[i] = *falseMap;
+      
+    }
+  }
 
-	/* build planMaps variables by accessing OP_map_list with indexes(=positions) in args */
-	for ( i = 0; i < argsNumber; i++ )
-	{
-		op_map_core * tmp;
-		int j;
+  /* build dimensions of data using op_dat */
+  for ( i = 0; i < argsNumber; i++ )
+  {
+    planDims[i] = planDatArgs[i].dim;
+  }
 
-		if ( inds[i] >= 0 ) /* another magic number !!! */
-		{
-			int iter;
-			tmp = OP_map_list[maps[i]];
-			planMaps[i] = *tmp;
-		}
-		else
-		{
-			/* build false map with index = -1 ... */
-		  op_map_core * falseMap = (op_map_core *) calloc ( 1, sizeof ( op_map_core ) ); //OP_ID;
-		  falseMap->index = -1;
-		  planMaps[i] = *falseMap;
-		  
-		}
-	}
+  /* build op_dat data type names (allocate precise space for name and copy it) */
+  for ( i = 0; i < argsNumber; i++ )
+  {
+    /* obtain reference to next op_dat */
+    op_dat_core * tmpDat = OP_dat_list[args[i]];
 
-	/* build dimensions of data using op_dat */
-	for ( i = 0; i < argsNumber; i++ )
-	{
-		planDims[i] = planDatArgs[i].dim;
-	}
+    /* allocate space and copy strings */
+    int typeNameLen = strlen ( tmpDat->type );
 
-	/* build op_dat data type names (allocate precise space for name and copy it) */
-	for ( i = 0; i < argsNumber; i++ )
-	{
-		/* obtain reference to next op_dat */
-		op_dat_core * tmpDat = OP_dat_list[args[i]];
+    planTypes[i] = (char * ) calloc ( typeNameLen, sizeof ( char ) );
+    strncpy ( planTypes[i], tmpDat->type, typeNameLen );
+  }
 
-		/* allocate space and copy strings */
-		int typeNameLen =	strlen ( tmpDat->type );
-
-		planTypes[i] = (char * ) calloc ( typeNameLen, sizeof ( char ) );
-		strncpy ( planTypes[i], tmpDat->type, typeNameLen );
-	}
-
-	/* build op_access array needed to build the plan */
-	for ( i = 0; i < argsNumber; i++ )
-	{
-		planAccs[i] = getAccFromIntCode ( accs[i] );
-	}
+  /* build op_access array needed to build the plan */
+  for ( i = 0; i < argsNumber; i++ )
+  {
+    planAccs[i] = getAccFromIntCode ( accs[i] );
+  }
 
   /* now builds op_arg array */
   for ( i = 0; i < argsNumber; i++ )
@@ -163,22 +150,50 @@ op_plan * FortranPlanCaller ( char name[],
       exit ( 0 );
     }
   }
-
-
-  generatedPlan = op_plan_get ( name,
-						   iterationSet,
-						   partitionSize,
-               argsNumber,
-               planArguments,
-						   indsNumber,
-						   inds
-						 );
-
-
-
-
-  return generatedPlan;
-
+  
+  return planArguments;
 }
 
 
+/*
+ * Wrapper for Fortran to plan function for OP2 --> OpenMP
+ */
+
+op_plan * FortranPlanCallerOpenMP ( char name[],
+                                    int setId,
+                                    int argsNumber,
+                                    int args[],
+                                    int idxs[],
+                                    int maps[],
+                                    int accs[],
+                                    int indsNumber,
+                                    int inds[],
+                                    int argsType[],
+                                    int partitionSize
+                                  )
+{
+  op_plan * generatedPlan = NULL;
+  op_set_core * iterationSet =  OP_set_list[setId];
+  
+  if ( iterationSet == NULL )
+  {
+     /* TO DO: treat this as an error to be returned to the caller */
+    printf ( "bad set index\n" );
+    exit ( -1 );
+  }
+  
+  /* generate the input arguments for the plan function */
+  op_arg * planArguments = generatePlanInputData ( name, setId, argsNumber, args, idxs, maps, accs, indsNumber, inds, argsType );
+
+  /* call the C OP2 core function (we don't need anything else for openmp */
+  generatedPlan = op_plan_core ( name,
+                                 iterationSet,
+                                 partitionSize,
+                                 argsNumber,
+                                 planArguments,
+                                 indsNumber,
+                                 inds
+                               );
+
+  return generatedPlan;
+}
