ecee9026508c375dc992146da03610dd1bca8af9
Dr. Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-06-28 12:45:51 +0100

    added debug file ptinout after save_soln

--- apps/fortran/airfoil/hand-made/openmp/airfoil.f90
+++ apps/fortran/airfoil/hand-made/openmp/airfoil.f90
@@ -198,6 +198,20 @@ program airfoil
 																		     & )
 
 
+    op_fetchdata ( p_qold )
+
+    retdebug = openfile ( c_char_"/work/cbertoll/OP2/qold-int.txt"//c_null_char )
+  
+    do debugiter = 1, 4*ncell
+  
+      datad = qold(debugiter)
+      retdebug = writerealtofile ( datad )
+    end do
+  
+    retdebug = closefile ()
+
+    stop
+
 		save_soln_host_time = save_soln_host_time + save_soln_info%hostTime
 		save_soln_count = save_soln_count + 1
 
