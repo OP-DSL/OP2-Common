862a03a4f613584c253558e8c19c020826227e2a
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-02-02 14:43:15 +0000

    Fix laplace1d example

--- apps/c/laplace1d/laplace.h
+++ apps/c/laplace1d/laplace.h
@@ -1,5 +1,5 @@
-void laplace(Real A[2][2], const Real *x) {
-  const Real hinv = 1./(x[1] - x[0]);
+void laplace(Real A[2][2], const Real *x[1]) {
+  const Real hinv = 1./(x[1][0] - x[0][0]);
   A[0][0] = hinv;
   A[0][1] = -hinv;
   A[1][0] = -hinv;
