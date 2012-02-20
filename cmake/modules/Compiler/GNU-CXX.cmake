aa150988bd4aa4be6e445118c6a9fd99834836da
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-20 20:36:26 +0000

    Override initial flags for GNU compilers

---
+++ cmake/modules/Compiler/GNU-CXX.cmake
@@ -0,0 +1,10 @@
+message("-- Setting GNU CXX compiler options")
+include(Compiler/GNU)
+__compiler_gnu(CXX)
+
+SET(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -std=c++98")
+
+# Custom Developer build type, need to create cache variable for that
+# ignore remark #981: operands are evaluated in unspecified order
+set(CMAKE_CXX_FLAGS_DEVELOPER "-O2 -g -Wall -Werror -pedantic -pipe" CACHE STRING
+  "Flags used by the compiler during Developer builds.")
