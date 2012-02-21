d08761e43a16844658ed38b6e0975bd5e1a7e635
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-21 13:43:25 +0000

    Override initial flags for PGI compilers

--- cmake/modules/Compiler/GNU-C.cmake
+++ cmake/modules/Compiler/GNU-C.cmake
@@ -5,6 +5,5 @@ __compiler_gnu(C)
 SET(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -std=c99")
 
 # Custom Developer build type, need to create cache variable for that
-# ignore remark #981: operands are evaluated in unspecified order
 set(CMAKE_C_FLAGS_DEVELOPER "-O2 -g -Wall -Werror -pedantic -pipe" CACHE STRING
   "Flags used by the compiler during Developer builds.")
---
+++ cmake/modules/Compiler/PGI-C.cmake
@@ -0,0 +1,9 @@
+include(Compiler/PGI)
+__compiler_pgi(C)
+SET(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -c99")
+set(CMAKE_C_FLAGS_MINSIZEREL_INIT "${CMAKE_C_FLAGS_MINSIZEREL_INIT} -DNDEBUG")
+set(CMAKE_C_FLAGS_RELEASE_INIT "${CMAKE_C_FLAGS_RELEASE_INIT} -fastsse -Minline=levels:10 -Mvect -Minfo -DNDEBUG")
+
+# Custom Developer build type, need to create cache variable for that
+set(CMAKE_C_FLAGS_DEVELOPER "-O2 -gopt -Minform=inform" CACHE STRING
+  "Flags used by the compiler during Developer builds.")
