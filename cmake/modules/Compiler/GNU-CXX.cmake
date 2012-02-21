7e9b655f317b4a5ed35efd77990bad54a84a276d
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-21 13:43:25 +0000

    Override initial flags for PGI compilers

--- cmake/modules/Compiler/GNU-CXX.cmake
+++ cmake/modules/Compiler/GNU-CXX.cmake
@@ -5,6 +5,5 @@ __compiler_gnu(CXX)
 SET(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -std=c++98")
 
 # Custom Developer build type, need to create cache variable for that
-# ignore remark #981: operands are evaluated in unspecified order
 set(CMAKE_CXX_FLAGS_DEVELOPER "-O2 -g -Wall -Werror -pedantic -pipe" CACHE STRING
   "Flags used by the compiler during Developer builds.")
