24f03595ac86d14b13b2e70ab3804378bff7ca0e
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-24 15:44:01 +0000

    Don't create op2.conf, generate input grid with naca grid generator.

--- apps/c/common.cmake
+++ apps/c/common.cmake
@@ -55,4 +55,47 @@ if(NOT OP2_ALL_APPS)
   include_directories(${OP2_INCLUDE_DIRS})
   add_definitions(${OP2_USER_DEFINITIONS})
 
-endif()
+  # Generate the airfoil input grid using the NACA grid generator
+  option(OP2_GENERATE_MESH "Generate input meshes during the build process."  ON)
+  # Skip if the grid target already exists (i.e. this has already been run)
+  if(OP2_GENERATE_MESH AND NOT TARGET grid)
+
+    set(GRID_FILE ${CMAKE_BINARY_DIR}/new_grid.dat)
+    find_file(GRID_GENERATOR naca0012.m PATHS ${CMAKE_CURRENT_SOURCE_DIR}/../../mesh_generators)
+
+    # Use Octave if available, otherwise fall back to MATLAB
+    find_program(OCTAVE_EXECUTABLE octave hints ${OCTAVE_DIR} ENV OCTAVE_DIR)
+    if(NOT OCTAVE_EXECUTABLE)
+      find_program(MATLAB_EXECUTABLE matlab hints ${MATLAB_DIR} ENV MATLAB_DIR)
+      if(NOT MATLAB_EXECUTABLE)
+        message(WARNING "Could not find Octave or MATLAB. Set OCTAVE_DIR and/or MATLAB_DIR to the folder(s) containing the executable(s).
+
+Automatic generation of the input mesh is skipped. Generate it manually by running:
+  ${GRID_GENERATOR}")
+      else()
+        message(STATUS "Generating input mesh with MATLAB")
+        set(GENERATE_GRID_CMD "${MATLAB_EXECUTABLE} -nodisplay -nojvm -nodesktop -nosplash -r 'naca0012 old;exit'")
+      endif()
+    else()
+      message(STATUS "Generating input mesh with Octave")
+      set(GENERATE_GRID_CMD ${OCTAVE_EXECUTABLE} --eval "naca0012")
+    endif()
+
+    if(GENERATE_GRID_CMD)
+      # Custom command to generate the grid
+      add_custom_command(OUTPUT ${GRID_FILE}
+        COMMAND ${CMAKE_COMMAND} -E copy ${GRID_GENERATOR} .
+        COMMAND ${GENERATE_GRID_CMD}
+        MAIN_DEPENDENCY ${GRID_GENERATOR}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Generating the input grid ${GRID_FILE}...")
+      install(FILES ${GRID_FILE} DESTINATION ${OP2_APPS_DIR} COMPONENT RuntimeInputFiles)
+      # We need a (global) custom target since output of a custom command can
+      # be used as a dependency only within the same file. This also makes
+      # sure the grid is only generated once if called from multiple files.
+      add_custom_target(grid DEPENDS ${GRID_FILE})
+    endif()
+
+  endif(OP2_GENERATE_MESH AND NOT TARGET grid)
+
+endif(NOT OP2_ALL_APPS)
