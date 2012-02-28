9b9a98e7685be67c3b064b0fe1f4e8ddb42ddde7
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-02-28 13:46:10 +0000

    Add DOLFIN copyright header to Find{ScotchPT,ParMETIS}.cmake

--- cmake/modules/FindParMETIS.cmake
+++ cmake/modules/FindParMETIS.cmake
@@ -11,22 +11,52 @@
 #  PARMETIS_SKIP_TESTS   - Skip tests building and running a test
 #                          executable linked against ParMETIS libraries
 
+#=============================================================================
+# Copyright (C) 2010 Garth N. Wells, Anders Logg, Johannes Ring and
+#   Florian Rathgeber
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
 if (MPI_FOUND)
   find_path(PARMETIS_INCLUDE_DIRS parmetis.h
     HINTS ${PARMETIS_INCLUDE_DIR} ${PARMETIS_DIR}/include
-    $ENV{PARMETIS_DIR}/include $ENV{PARMETIS_DIR} $ENV{PARMETIS_INCLUDE_DIR}
+      $ENV{PARMETIS_DIR}/include $ENV{PARMETIS_DIR} $ENV{PARMETIS_INCLUDE_DIR}
     DOC "Directory where the ParMETIS header files are located"
   )
 
   find_library(PARMETIS_LIBRARY parmetis
     HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
-    $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
+      $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
     DOC "Directory where the ParMETIS library is located"
   )
 
   find_library(METIS_LIBRARY metis
     HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
-    $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
+      $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
     DOC "Directory where the METIS library is located"
   )
 
