0a2ead449cb2eca005f69e61656922aa3e2aef89
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-03-02 18:30:49 +0000

    Find MPI instead of relying on MPI_FOUND to be set

--- cmake/modules/FindParMETIS.cmake
+++ cmake/modules/FindParMETIS.cmake
@@ -47,33 +47,34 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
 
-if (MPI_FOUND)
-  find_path(PARMETIS_INCLUDE_DIRS parmetis.h
-    HINTS ${PARMETIS_INCLUDE_DIR} ${PARMETIS_DIR}/include
+find_path(PARMETIS_INCLUDE_DIRS parmetis.h
+  HINTS ${PARMETIS_INCLUDE_DIR} ${PARMETIS_DIR}/include
     $ENV{PARMETIS_DIR}/include $ENV{PARMETIS_DIR} $ENV{PARMETIS_INCLUDE_DIR}
-    DOC "Directory where the ParMETIS header files are located"
-  )
+  DOC "Directory where the ParMETIS header files are located"
+)
 
-  find_library(PARMETIS_LIBRARY
-    NAMES parmetis parmetis${PARMETIS_LIB_SUFFIX}
-    HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
+find_library(PARMETIS_LIBRARY
+  NAMES parmetis parmetis${PARMETIS_LIB_SUFFIX}
+  HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
     $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
-    DOC "Directory where the ParMETIS library is located"
-  )
+  DOC "Directory where the ParMETIS library is located"
+)
 
-  find_library(METIS_LIBRARY
-    NAMES metis metis${PARMETIS_LIB_SUFFIX}
-    HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
+find_library(METIS_LIBRARY
+  NAMES metis metis${PARMETIS_LIB_SUFFIX}
+  HINTS ${PARMETIS_LIB_DIR} ${PARMETIS_DIR}/lib
     $ENV{PARMETIS_DIR}/lib $ENV{PARMETIS_DIR} $ENV{PARMETIS_LIB_DIR}
-    DOC "Directory where the METIS library is located"
-  )
+  DOC "Directory where the METIS library is located"
+)
 
-  set(PARMETIS_LIBRARIES ${PARMETIS_LIBRARY} ${METIS_LIBRARY})
+set(PARMETIS_LIBRARIES ${PARMETIS_LIBRARY} ${METIS_LIBRARY})
 
-  # Try compiling and running test program if not cross-compiling
-  if (PARMETIS_INCLUDE_DIRS AND PARMETIS_LIBRARY AND METIS_LIBRARY
-      AND NOT (CMAKE_CROSSCOMPILING OR PARMETIS_SKIP_TESTS))
+# Try compiling and running test program if not cross-compiling
+if (PARMETIS_INCLUDE_DIRS AND PARMETIS_LIBRARY AND METIS_LIBRARY
+    AND NOT (CMAKE_CROSSCOMPILING OR PARMETIS_SKIP_TESTS))
 
+  find_package(MPI QUIET)
+  if(MPI_FOUND)
     # Set flags for building test program
     set(CMAKE_REQUIRED_INCLUDES ${PARMETIS_INCLUDE_DIRS} ${MPI_INCLUDE_PATH})
     set(CMAKE_REQUIRED_LIBRARIES ${PARMETIS_LIBRARIES}  ${MPI_LIBRARIES})
@@ -97,12 +98,11 @@ int main()
   return 0;
 }
 " PARMETIS_TEST_RUNS)
-
   endif()
-  # When cross compiling assume tests have run successfully
-  if (CMAKE_CROSSCOMPILING OR PARMETIS_SKIP_TESTS)
-    set(PARMETIS_TEST_RUNS TRUE)
-  endif()
+endif()
+# When cross compiling assume tests have run successfully
+if (CMAKE_CROSSCOMPILING OR PARMETIS_SKIP_TESTS)
+  set(PARMETIS_TEST_RUNS TRUE)
 endif()
 
 # Standard package handling
