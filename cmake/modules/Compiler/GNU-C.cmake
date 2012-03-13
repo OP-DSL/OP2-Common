d60c8208fe631bd8c096038e9eee9042fd2c49e5
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-03-13 12:02:23 +0000

    Update sequential vector-map examples for new build system

--- cmake/modules/Compiler/GNU-C.cmake
+++ cmake/modules/Compiler/GNU-C.cmake
@@ -5,6 +5,6 @@ __compiler_gnu(C)
 SET(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -std=c99")
 
 # Custom Developer build type, need to create cache variable for that
-set(CMAKE_C_FLAGS_DEVELOPER "-O2 -g -Wall -Werror -pedantic -pipe" CACHE STRING
+set(CMAKE_C_FLAGS_DEVELOPER "-O0 -g -Wall -Werror -pedantic -pipe" CACHE STRING
   "Flags used by the compiler during Developer builds.")
 mark_as_advanced(CMAKE_C_FLAGS_DEVELOPER)
