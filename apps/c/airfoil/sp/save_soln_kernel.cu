f932ccb244d2fffbe38b834e73593ecf54de33e7
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2012-03-15 15:56:27 +0000

    Updated kernel files with timers_core

--- apps/c/airfoil/sp/save_soln_kernel.cu
+++ apps/c/airfoil/sp/save_soln_kernel.cu
@@ -77,7 +77,7 @@ void op_par_loop_save_soln(char const *name, op_set set,
   // initialise timers                                                    
                                                                           
   double cpu_t1, cpu_t2, wall_t1, wall_t2;                                
-  op_timers(&cpu_t1, &wall_t1);                                           
+  op_timers_core(&cpu_t1, &wall_t1);                                           
                                                                           
   if (set->size >0) {                                                     
                                                                           
--- apps/c/airfoil_hdf5/dp/update_kernel.cu
+++ apps/c/airfoil_hdf5/dp/update_kernel.cu
@@ -107,7 +107,7 @@ void op_par_loop_update(char const *name, op_set set,
   // initialise timers                                                  
                                                                         
   double cpu_t1, cpu_t2, wall_t1, wall_t2;                              
-  op_timers(&cpu_t1, &wall_t1);                                         
+  op_timers_core(&cpu_t1, &wall_t1);                                         
                                                                         
   if (set->size >0) {                                                   
                                                                         
@@ -183,8 +183,7 @@ void op_par_loop_update(char const *name, op_set set,
                                                                         
   // update kernel record                                               
                                                                         
-  op_mpi_barrier();                                                     
-  op_timers(&cpu_t2, &wall_t2);                                         
+  op_timers_core(&cpu_t2, &wall_t2);                                        
   op_timing_realloc(4);                                                 
   OP_kernels[4].name      = name;                                       
   OP_kernels[4].count    += 1;                                          
