1e7a4623ad2f3abec4973e49d1014ee1c53673c6
Carlo Bertolli <cbertoll@login-0.cx1.hpc.ic.ac.uk>
2012-03-20 17:56:26 +0000

    Added set_offset to interface between Fortran OpenMP and C

--- op2/fortran/src/backend/op2_for_rt_support.f90
+++ op2/fortran/src/backend/op2_for_rt_support.f90
@@ -24,6 +24,7 @@ module OP2_Fortran_RT_Support
     type(c_ptr) ::                         nthrcol ! number of thread colors for each block
     type(c_ptr) ::                         thrcol ! thread colors
     type(c_ptr) ::                         offset ! offset for primary set
+    integer(kind=c_int) ::                 set_offset ! offset inside set (for MPI back-ends)
     type(c_ptr) ::                            ind_maps ! pointers for indirect datasets
     type(c_ptr) ::                         ind_offs ! offsets for indirect datasets
     type(c_ptr) ::                         ind_sizes ! offsets for indirect datasets
