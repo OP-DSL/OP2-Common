3cf0c7ffe310ad89bde075ea80a83fd4cdf489c8
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-03-23 13:06:31 +0000

    Update mass2d example to follow op_iteration_space model
    Single-matrix-entry assembly is the only semantic we support, so
    remove the mass-multiset version.  We now have to specify the
    iteration space explicitly in the op_par_loop, so do that.

--- apps/c/mass2d/mass.cpp
+++ apps/c/mass2d/mass.cpp
@@ -10,41 +10,41 @@
 
 int main(int argc, char **argv)
 {
-    int *p_elem_node;
-    double *p_xn;
+  int *p_elem_node;
+  float *p_xn;
 
-    op_init(argc, argv, 5);
+  op_init(argc, argv, 5);
 
-    p_elem_node = (int *)malloc(3 * NUM_ELE * sizeof(int));
-    p_elem_node[0] = 0;
-    p_elem_node[1] = 1;
-    p_elem_node[2] = 3;
-    p_elem_node[3] = 2;
-    p_elem_node[4] = 3;
-    p_elem_node[5] = 1;
+  p_elem_node = (int *)malloc(3 * NUM_ELE * sizeof(int));
+  p_elem_node[0] = 0;
+  p_elem_node[1] = 1;
+  p_elem_node[2] = 3;
+  p_elem_node[3] = 2;
+  p_elem_node[4] = 3;
+  p_elem_node[5] = 1;
 
-    p_xn = (double *)malloc(2 * NUM_NODES * sizeof(double));
-    p_xn[0] = 0.0f;
-    p_xn[1] = 0.0f;
-    p_xn[2] = 2.0f;
-    p_xn[3] = 0.0f;
-    p_xn[4] = 1.0f;
-    p_xn[5] = 1.0f;
-    p_xn[6] = 0.0f;
-    p_xn[7] = 1.5f;
+  p_xn = (float *)malloc(2 * NUM_NODES * sizeof(float));
+  p_xn[0] = 0.0f;
+  p_xn[1] = 0.0f;
+  p_xn[2] = 2.0f;
+  p_xn[3] = 0.0f;
+  p_xn[4] = 1.0f;
+  p_xn[5] = 1.0f;
+  p_xn[6] = 0.0f;
+  p_xn[7] = 1.5f;
 
-    op_set nodes = op_decl_set(NUM_NODES, "nodes");
-    op_set elements = op_decl_set(NUM_ELE, "elements");
-    op_map elem_node = op_decl_map(elements, nodes, 3, p_elem_node, "elem_node");
+  op_set nodes = op_decl_set(NUM_NODES, "nodes");
+  op_set elements = op_decl_set(NUM_ELE, "elements");
+  op_map elem_node = op_decl_map(elements, nodes, 3, p_elem_node, "elem_node");
 
-    op_sparsity sparsity = op_decl_sparsity(elem_node, elem_node, "sparsity");
-    op_mat mat = op_decl_mat(sparsity, 1, "double", sizeof(double), "mat");
-    op_dat xn = op_decl_dat(nodes, 2, "double", p_xn, "xn");
+  op_sparsity sparsity = op_decl_sparsity(elem_node, elem_node, "sparsity");
+  op_mat mat = op_decl_mat(sparsity, 1, "float", sizeof(float), "mat");
+  op_dat xn = op_decl_dat(nodes, 2, "float", p_xn, "xn");
 
-    op_par_loop(mass, "sum", elements,
-                op_arg_mat(mat, -3, elem_node, -3, elem_node, 1, "double", OP_INC),
-                op_arg_dat(xn, -3, elem_node, 2, "double", OP_READ));
+  op_par_loop(mass, "mass", op_iteration_space(elements, 3, 3),
+              op_arg_mat(mat, op_i(1), elem_node, op_i(2), elem_node, 1, "float", OP_INC),
+              op_arg_dat(xn, -3, elem_node, 2, "float", OP_READ));
 
-    op_exit();
-    return 0;
+  op_exit();
+  return 0;
 }
