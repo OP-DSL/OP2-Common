c41221cc424e54377b88ad09a657dcc170973810
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-03-23 12:45:01 +0000

    Free memory associated with PETSc matrix handle on exit

--- op2/c/src/sequential/op_seq.c
+++ op2/c/src/sequential/op_seq.c
@@ -31,7 +31,7 @@
  */
 
 #include <petscsys.h>
-
+#include <petscmat.h>
 #include <op_lib_c.h>
 
 /*
@@ -112,6 +112,11 @@ void op_timers(double * cpu, double * et)
 
 void op_exit ()
 {
+  for ( int i = 0; i < OP_mat_index; i++ )
+  {
+      MatDestroy ( ((Mat)(OP_mat_list[i]->mat)) );
+  }
+
   op_exit_core ();
   PetscFinalize();
 }
