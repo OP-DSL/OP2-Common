1c16ce0d9cfc96a6f81b61fa79fe394559ab3631
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-05-02 09:41:44 +0100

    Extend op2_application to handle multiple backends and make it a function

--- cmake/modules/OP2Application.cmake
+++ cmake/modules/OP2Application.cmake
@@ -4,48 +4,59 @@ if(DEFINED __OP2_APPLICATION_INCLUDED)
 endif()
 set(__OP2_APPLICATION_INCLUDED TRUE)
 
-macro(op2_application APP BACKEND)
+include(CMakeParseArguments)
+
+function(op2_application APP)
+  # Parse arguments
+  cmake_parse_arguments(${APP} "" "" "BACKENDS;SOURCES" ${ARGN})
+
   # Preparation
-  if (${BACKEND} STREQUAL SEQUENTIAL)
-    set(LIB op2_seq)
-  elseif (${BACKEND} STREQUAL CUDA)
-    set(LIB op2_cuda)
-  elseif (${BACKEND} STREQUAL OPENMP)
-    set(LIB op2_openmp)
-  elseif (${BACKEND} STREQUAL MPI)
-    set(LIB op2_mpi)
-    add_definitions(${OP2_MPI_DEFINITIONS})
-    include_directories(${OP2_MPI_INCLUDE_DIRS})
-  elseif (${BACKEND} STREQUAL HDF5)
-    set(LIB op2_hdf5)
-  else()
-    message(WARNING "Invalid backend ${BACKEND} for application ${APP}")
-  endif()
+  foreach (BACKEND ${${APP}_BACKENDS})
+    if (${BACKEND} STREQUAL SEQUENTIAL)
+      set(LIB op2_seq)
+    elseif (${BACKEND} STREQUAL CUDA)
+      find_package(CUDA)
+      if (NOT CUDA_FOUND)
+        return()
+      endif()
+      set(LIB op2_cuda)
+      set(CUDA_ENABLED TRUE)
+    elseif (${BACKEND} STREQUAL OPENMP)
+      set(LIB op2_openmp)
+      set(OPENMP_ENABLED TRUE)
+    elseif (${BACKEND} STREQUAL MPI)
+      set(LIB op2_mpi)
+      add_definitions(${OP2_MPI_DEFINITIONS})
+      include_directories(${OP2_MPI_INCLUDE_DIRS})
+    elseif (${BACKEND} STREQUAL HDF5)
+      set(LIB op2_hdf5)
+    else()
+      message(WARNING "Invalid backend ${BACKEND} for application ${APP}")
+    endif()
 
-  # Skip if the required library hasn't been build
-  if (NOT TARGET ${LIB})
-    return()
-  endif()
-  message(STATUS "Configuring application ${APP} for the ${BACKEND} backend")
-
-  # Add the executable
-  if (${BACKEND} STREQUAL CUDA)
-    find_package(CUDA)
-    if (NOT CUDA_FOUND)
+    # Skip if the required library hasn't been build
+    if (NOT TARGET ${LIB})
       return()
     endif()
-    set(LIB op2_cuda)
-    cuda_add_executable(${APP} ${ARGN})
+    # Otherwise add to library set
+    set(LIBS ${LIBS} ${LIB})
+  endforeach()
+
+  message("${APP} ${${APP}_BACKENDS} ${${APP}_SOURCES} ${LIBS}")
+
+  # Add the executable
+  if (CUDA_ENABLED)
+    cuda_add_executable(${APP} ${${APP}_SOURCES})
   else()
-    add_executable(${APP} ${ARGN})
+    add_executable(${APP} ${${APP}_SOURCES})
   endif()
 
-  if (${BACKEND} STREQUAL OPENMP)
-    set_target_properties(airfoil_${VARIANT}_openmp PROPERTIES COMPILE_FLAGS
+  if (OPENMP_ENABLED)
+    set_target_properties(${APP} PROPERTIES COMPILE_FLAGS
       "${OpenMP_CXX_FLAGS}" LINK_FLAGS "${OpenMP_CXX_FLAGS}")
   endif()
 
   add_dependencies(${APP} grid)
-  target_link_libraries(${APP} ${LIB})
+  target_link_libraries(${APP} ${LIBS})
   install(TARGETS ${APP} RUNTIME DESTINATION ${OP2_APPS_DIR} COMPONENT RuntimeExecutables)
-endmacro()
+endfunction()
