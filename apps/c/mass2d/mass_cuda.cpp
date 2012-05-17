86a0d046872b7c95bb9893d85fe4d733f799ed10
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-05-17 20:13:01 +0100

    First stab at implementing rhs for hand-written mass2D CUDA example

--- apps/c/mass2d/mass_cuda.cpp
+++ apps/c/mass2d/mass_cuda.cpp
@@ -11,6 +11,8 @@
 
 void op_par_loop_mass(const char *, op_set, op_arg, op_arg);
 
+void op_par_loop_rhs(const char *, op_set, op_arg, op_arg, op_arg);
+
 void read_from_triangle_files(std::string basename,
                               int *nnode,
                               int *nele,
@@ -59,8 +61,7 @@ void read_from_triangle_files(std::string basename,
 int main(int argc, char **argv)
 {
   int *p_elem_node;
-  ValueType *p_xn;
-  int i;
+  ValueType *p_xn, *p_b, *p_f, *p_x;
   ValueType val;
 
   op_init(argc, argv, 5);
@@ -88,6 +89,18 @@ int main(int argc, char **argv)
     p_xn[5] = 1.0f;
     p_xn[6] = 0.0f;
     p_xn[7] = 1.5f;
+
+    p_f = (ValueType*)malloc(nnode * sizeof(ValueType));
+    p_f[0] = 1.0f;
+    p_f[1] = 2.0f;
+    p_f[2] = 3.0f;
+    p_f[3] = 4.0f;
+
+    p_b = (ValueType*)malloc(nnode * sizeof(ValueType));
+    memset(p_b, 0, nnode * sizeof(ValueType));
+
+    p_x = (ValueType*)malloc(nnode * sizeof(ValueType));
+    memset(p_x, 0, nnode * sizeof(ValueType));
   }
   op_set nodes = op_decl_set(nnode, "nodes");
   op_set elements = op_decl_set(nele, "elements");
@@ -97,10 +110,37 @@ int main(int argc, char **argv)
   op_mat mat = op_decl_mat(sparsity, 1, VALUESTR, sizeof(ValueType), "mat");
   op_dat xn = op_decl_dat(nodes, 2, VALUESTR, p_xn, "xn");
 
+  // Dat for the field initial condition
+  op_dat f = op_decl_dat(nodes, 1, VALUESTR, p_f, "f");
+
+  // Dat for the RHS vector
+  op_dat b = op_decl_dat(nodes, 1, VALUESTR, p_b, "b");
+
+  // Dat for solution
+  op_dat x = op_decl_dat(nodes, 1, VALUESTR, p_x, "x");
+
   op_par_loop_mass("mass", elements,
                    op_arg_mat(mat, -3, elem_node, -3, elem_node, 1, VALUESTR, OP_INC),
                    op_arg_dat(xn, -3, elem_node, 2, VALUESTR, OP_READ));
 
+  op_par_loop_rhs("rhs", elements,
+                  op_arg_dat(b, -3, elem_node, 1, VALUESTR, OP_INC),
+                  op_arg_dat(xn, -3, elem_node, 2, VALUESTR, OP_READ),
+                  op_arg_dat(f, -3, elem_node, 1, VALUESTR, OP_READ));
+
+  op_solve(mat, b, x);
+
+  // Check result
+  int failed = 0;
+  op_fetch_data(x);
+  for (int i=0; i<nnode; ++i) {
+    double delta = fabs((double)p_x[i] - (double)p_f[i]);
+    if (delta > TOLERANCE) {
+      failed = 1;
+      printf("Failed: delta = %18.16f for node %d.\n", delta, i);
+    }
+  }
+
   op_exit();
-  return 0;
+  return failed;
 }
