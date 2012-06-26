1b5a409cb6529a640e6b525e2b270166c1b867a2
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2012-06-26 11:37:45 +0100

    Removing airfoil_mpi.cpp from hdf5 directory as it is not needed for the build process

--- apps/c/airfoil/airfoil_hdf5/dp/airfoil.cpp
+++ apps/c/airfoil/airfoil_hdf5/dp/airfoil.cpp
@@ -212,6 +212,12 @@ int main(int argc, char **argv)
   }
 
   op_timers(&cpu_t2, &wall_t2);
+  
+  //output the result dat array to files
+  //op_write_hdf5("new_grid_out.h5");
+
+  //compress using
+  // ~/hdf5/bin/h5repack -f GZIP=9 new_grid.h5 new_grid_pack.h5
 
   op_timing_output();
   op_printf("Max total runtime = \n%f\n",wall_t2-wall_t1);
