4dab40927def79498097697ca0fccff0658c9f4b
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-07-31 15:05:44 +0100

    Properly clean up in op_exit and op_rt_exit

--- op2/c/src/core/op_lib_core.c
+++ op2/c/src/core/op_lib_core.c
@@ -422,6 +422,7 @@ op_exit_core (  )
     free ( OP_mat_list[i] );
   }
   free ( OP_mat_list );
+  OP_mat_list = NULL;
 
   for ( int i = 0; i < OP_sparsity_index; i++ )
   {
@@ -431,6 +432,7 @@ op_exit_core (  )
     free ( OP_sparsity_list[i] );
   }
   free ( OP_sparsity_list );
+  OP_sparsity_list = NULL;
 
   // free storage for timing info
 
