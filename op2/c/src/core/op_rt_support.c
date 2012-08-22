56fe7c31ef22a3608be9c04a738f9d8c945939fb
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2012-08-22 15:23:22 +0100

    Removing op_dat->dat comparison in plan construction

--- op2/c/src/core/op_rt_support.c
+++ op2/c/src/core/op_rt_support.c
@@ -331,7 +331,8 @@ op_plan *op_plan_core(char const *name, op_set set, int part_size,
       match = 1;
       for ( int m = 0; m < nargs; m++ )
       {
-        match = match && ( args[m].dat == OP_plans[ip].dats[m] )
+        match = match
+        /*&& ( args[m].dat == OP_plans[ip].dats[m] ) -- to allow temp dats*/
           && ( args[m].map == OP_plans[ip].maps[m] )
           && ( args[m].idx == OP_plans[ip].idxs[m] )
           && ( args[m].acc == OP_plans[ip].accs[m] );
