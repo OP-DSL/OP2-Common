30f57108594621edb6515e668e76cbc555a6dda7
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-08-24 14:24:12 +0100

    Avoid segfaults if halos were never initialised
    Make op_halo_destroy and op_mpi_halo_exchanges a no-op if there are no
    halos.  Tested for by checking if OP_import_exec_list is NULL.

--- op2/c/src/mpi/op_mpi_core.c
+++ op2/c/src/mpi/op_mpi_core.c
@@ -2200,7 +2200,12 @@ void op_mpi_exit()
   }
 #endif
 
-  //free memory allocated to halos and mpi_buffers
+  /* Don't free if we didn't build halos */
+  if (!OP_import_exec_list)
+  {
+    return;
+  }
+  //free memory allocated to halos
   op_halo_destroy();
   //return all op_dats, op_maps back to original element order
   op_partition_reverse();
@@ -2210,6 +2215,11 @@ void op_mpi_exit()
 
 int op_mpi_halo_exchanges(op_set set, int nargs, op_arg *args) {
   int size = set->size;
+  /* No halos */
+  if (!OP_import_exec_list)
+  {
+    return size;
+  }
   for (int n=0; n<nargs; n++) {
     if(args[n].argtype == OP_ARG_DAT)
     {
