048038b2bd50a8bd1f680569a139e6914b5c4c8e
Endre Laszlo <laszlo.endre@gmail.com>
2012-10-11 15:27:50 +0100

    Added: OutputSimulation()-write snapshots to file

--- apps/c/volna/volna_output.cpp
+++ apps/c/volna/volna_output.cpp
@@ -1,6 +1,97 @@
 #include "volna_common.h"
 #include "getTotalVol.h"
 
+inline void WriteVTKAscii(const char* filename, op_dat nodeCoords, int nnode, op_map cellsToNodes, int ncell, op_dat values) {
+  FILE* fp;
+  fp = fopen(filename, "w");
+  if(fp == NULL) {
+    op_printf("can't open file for write %s\n",filename);
+    exit(-1);
+  }
+
+  // write header
+  fprintf(fp,"# vtk DataFile Version 2.0\n Output from OP2 Volna.\n");
+  fprintf(fp,"ASCII \nDATASET UNSTRUCTURED_GRID\n\n");
+
+  // write vertices
+  fprintf(fp,"POINTS %d float\n", nnode);
+
+  float* nodeCoords_data;
+  nodeCoords_data = (float*)nodeCoords->data;
+
+  int i = 0;
+//  for (i = 0; i < nodeCoords->size/nodeCoords->dim; ++i) {
+  for (i = 0; i < nnode; ++i) {
+    fprintf(fp, "%f %f %f \n",
+        (float)nodeCoords_data[i*MESH_DIM  ],
+        (float)nodeCoords_data[i*MESH_DIM+1],
+        0.0f);
+  }
+  fprintf(fp, "\n");
+
+  // write cells
+  fprintf(fp, "CELLS %d %d \n", ncell, 4*ncell);
+
+  for ( i = 0; i < ncell; ++i ) {
+    fprintf(fp, "3 %d %d %d \n",
+        cellsToNodes->map[i*N_NODESPERCELL  ]-1,
+        cellsToNodes->map[i*N_NODESPERCELL+1]-1,
+        cellsToNodes->map[i*N_NODESPERCELL+2]-1);
+  }
+  fprintf(fp, "\n");
+
+  // write cell types (5 for triangles)
+  fprintf(fp, "CELL_TYPES %d \n", ncell);
+
+  for ( i=0; i<ncell; ++i )
+    fprintf(fp, "5 \n");
+//    os << VTK_TYPE<3,2>::value() << "\n";
+  fprintf(fp, "\n");
+
+  float* values_data;
+  values_data = (float*) values->data;
+
+  fprintf(fp, "SCALARS Eta double 1\n"
+              "LOOKUP_TABLE default\n");
+  for ( i=0; i<ncell; ++i )
+    fprintf(fp, "%f \n", values_data[i*N_STATEVAR] + values_data[N_STATEVAR+3]);
+  fprintf(fp, "\n");
+
+  fprintf(fp, "SCALARS U double 1\n"
+              "LOOKUP_TABLE default\n");
+  for ( i=0; i<ncell; ++i )
+    fprintf(fp, "%f \n", values_data[i*N_STATEVAR+1]);
+  fprintf(fp, "\n");
+
+  fprintf(fp, "SCALARS V double 1\n"
+              "LOOKUP_TABLE default\n");
+  for ( i=0; i<ncell; ++i )
+    fprintf(fp, "%f \n", values_data[i*N_STATEVAR+2]);
+  fprintf(fp, "\n");
+
+  fprintf(fp, "SCALARS Bathymetry double 1\n"
+              "LOOKUP_TABLE default\n");
+  for ( i=0; i<ncell; ++i )
+    fprintf(fp, "%f \n", values_data[i*N_STATEVAR+3]);
+  fprintf(fp, "\n");
+
+
+  fprintf(fp, "SCALARS Visual double 1\n"
+              "LOOKUP_TABLE default\n");
+  for ( i=0; i<ncell; ++i ) {
+    if(values_data[i*N_STATEVAR] < 1e-3)
+      fprintf(fp, "%f \n", 100.0f);
+    else
+      fprintf(fp, "%f \n", values_data[i*N_STATEVAR] + values_data[N_STATEVAR+3]);
+  }
+  fprintf(fp, "\n");
+
+  if(fclose(fp) != 0) {
+    op_printf("can't close file %s\n",filename);
+    exit(-1);
+  }
+}
+
 void OutputTime(TimerParams *timer) {
   op_printf("Iteration: %d, time: %f \n", (*timer).iter, (*timer).t);
 }
@@ -15,35 +106,17 @@ void OutputConservedQuantities(op_set cells, op_dat cellVolumes, op_dat values) 
   op_printf("mass(volume): %f \n", totalVol);
 }
 
-//void OutputSimulation(op_set)
+void OutputSimulation(EventParams *event, TimerParams* timer, op_dat nodeCoords, op_map cellsToNodes, op_dat values) {
+  const char* filename = event->streamName.c_str();
+  op_printf("Write output to file: %s \n", filename);
+  int nnode = nodeCoords->set->size;
+  int ncell = cellsToNodes->from->size;
+  const char* substituteIndexPattern = "%i";
+  char* pos;
+  pos = strstr(filename, substituteIndexPattern);
+  char* substituteIndex;
+  sprintf(substituteIndex, "%04d.vtk", timer->iter);
+  strcpy(pos, substituteIndex);
+  WriteVTKAscii(filename, nodeCoords, nnode, cellsToNodes, ncell, values);
+}
 
-//void OutputSimulation::execute( Mesh &mesh, Values &V ) {
-//
-//  std::stringstream iterStream;
-//  iterStream << std::setfill('0') << std::setw(4) << timer.iter;
-//  std::string iterString = iterStream.str();
-//
-//  std::string filename = streamName;
-//
-//  std::string searchString = "\%i";
-//
-//  std::string::size_type pos = 0;
-//  while ( ( pos = filename.find( searchString, pos ) ) != string::npos ) {
-//    filename.replace( pos, searchString.size(), iterString );
-//    ++pos;
-//  }
-//
-//  std::ofstream stream( filename.c_str() );
-//
-//  if ( !stream.good() ) {
-//    std::cerr << "Warning: impossible to write in file.";
-//  }
-//
-//  mesh.WriteVTKAscii( stream );
-//
-//  stream << "CELL_DATA " << mesh.NVolumes << "\n";
-//  V.WriteVTKAscii( stream );
-//
-//  stream.close();
-//
-//}
