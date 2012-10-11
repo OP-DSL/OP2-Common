a63316b7924e5a6b6f6d5800b128e7dc20c703bd
Endre Laszlo <laszlo.endre@gmail.com>
2012-10-11 17:55:06 +0100

    Some modifications done

--- apps/c/volna/NumericalFluxes_1.h
+++ apps/c/volna/NumericalFluxes_1.h
@@ -1,18 +1,18 @@
-void NumericalFluxes_1(float *LeftFacetValues, //OP_READ
-           float *RightFacetValues, //OP_READ
+void NumericalFluxes_1(float *leftCellValues, //OP_READ
+           float *rightCellValues, //OP_READ
            float *out, //OP_WRITE
            float *FacetVolumes, //OP_READ,
            float *Normals, //OP_READ,
            float *maxFacetEigenvalues //OP_WRITE
 )
 {
-  float cL = sqrt(g * LeftFacetValues[0]);
+  float cL = sqrt(g * leftCellValues[0]);
   cL = cL > 0.0 ? cL : 0.0;
-  float cR = sqrt(g * RightFacetValues[0]);
+  float cR = sqrt(g * rightCellValues[0]);
   cR = cR > 0.0 ? cR : 0.0;
 
-  float uLn = LeftFacetValues[1] * Normals[0] + LeftFacetValues[2] * Normals[1];
-  float uRn = RightFacetValues[1] * Normals[0] + RightFacetValues[2] * Normals[1];
+  float uLn = leftCellValues[1] * Normals[0] + leftCellValues[2] * Normals[1];
+  float uRn = rightCellValues[1] * Normals[0] + rightCellValues[2] * Normals[1];
 
   float unStar = 0.5 * (uLn + uRn) - 0.25* (cL+cR);
   float cStar = 0.5 * (cL + cR) - 0.25* (uLn+uRn);
@@ -23,11 +23,11 @@ void NumericalFluxes_1(float *LeftFacetValues, //OP_READ
   float sR = (uRn + cR) > (unStar + cStar) ? (uRn + cR) : (unStar + cStar);
   float sRPlus = sR > 0.0 ? sR : 0.0;
 
-  sL = LeftFacetValues[0] < EPS ? uRn - 2.0*cR : sL; // is this 2.0 or 2? (i.e. float/int)
-  sR = LeftFacetValues[0] < EPS ? uRn + cR : sR;
+  sL = leftCellValues[0] < EPS ? uRn - 2.0*cR : sL; // is this 2.0 or 2? (i.e. float/int)
+  sR = leftCellValues[0] < EPS ? uRn + cR : sR;
 
-  sR = RightFacetValues[0] < EPS ? uLn + 2.0*cL : sR; // is this 2.0 or 2? (i.e. float/int)
-  sL = RightFacetValues[0] < EPS ? uLn - cL : sL;
+  sR = rightCellValues[0] < EPS ? uLn + 2.0*cL : sR; // is this 2.0 or 2? (i.e. float/int)
+  sL = rightCellValues[0] < EPS ? uLn - cL : sL;
 
   float sRMinussL = sRPlus - sLMinus;
   sRMinussL = sRMinussL < EPS ? EPS : sRMinussL;
@@ -41,48 +41,48 @@ void NumericalFluxes_1(float *LeftFacetValues, //OP_READ
   float t3 = ( sRPlus * sLMinus ) / sRMinussL;
 
   float LeftFluxes_H, LeftFluxes_U, LeftFluxes_V;
-  //inlined ProjectedPhysicalFluxes(LeftFacetValues, Normals, params, LeftFluxes);
-  float HuDotN = (LeftFacetValues[0] * LeftFacetValues[1] * Normals[0]) +
-          (LeftFacetValues[0] * LeftFacetValues[2] * Normals[1]);
+  //inlined ProjectedPhysicalFluxes(leftCellValues, Normals, params, LeftFluxes);
+  float HuDotN = (leftCellValues[0] * leftCellValues[1] * Normals[0]) +
+          (leftCellValues[0] * leftCellValues[2] * Normals[1]);
 
   LeftFluxes_H = HuDotN;
-  LeftFluxes_U = HuDotN * LeftFacetValues[1];
-  LeftFluxes_V = HuDotN * LeftFacetValues[2];
+  LeftFluxes_U = HuDotN * leftCellValues[1];
+  LeftFluxes_V = HuDotN * leftCellValues[2];
 
-  LeftFluxes_U += (.5 * g * Normals[0] ) * ( LeftFacetValues[0] * LeftFacetValues[0] );
-  LeftFluxes_V += (.5 * g * Normals[1] ) * ( LeftFacetValues[0] * LeftFacetValues[0] );
+  LeftFluxes_U += (.5 * g * Normals[0] ) * ( leftCellValues[0] * leftCellValues[0] );
+  LeftFluxes_V += (.5 * g * Normals[1] ) * ( leftCellValues[0] * leftCellValues[0] );
   //end of inlined
 
   float RightFluxes_H, RightFluxes_U, RightFluxes_V;
-  //inlined ProjectedPhysicalFluxes(RightFacetValues, Normals, params, RightFluxes);
-  HuDotN = (RightFacetValues[0] * RightFacetValues[1] * Normals[0]) +
-          (RightFacetValues[0] * RightFacetValues[2] * Normals[1]);
+  //inlined ProjectedPhysicalFluxes(rightCellValues, Normals, params, RightFluxes);
+  HuDotN = (rightCellValues[0] * rightCellValues[1] * Normals[0]) +
+          (rightCellValues[0] * rightCellValues[2] * Normals[1]);
 
   RightFluxes_H =   HuDotN;
-  RightFluxes_U =   HuDotN * RightFacetValues[1];
-  RightFluxes_V =   HuDotN * RightFacetValues[2];
+  RightFluxes_U =   HuDotN * rightCellValues[1];
+  RightFluxes_V =   HuDotN * rightCellValues[2];
 
-  RightFluxes_U += (.5 * g * Normals[0] ) * ( RightFacetValues[0] * RightFacetValues[0] );
-  RightFluxes_V += (.5 * g * Normals[1] ) * ( RightFacetValues[0] * RightFacetValues[0] );
+  RightFluxes_U += (.5 * g * Normals[0] ) * ( rightCellValues[0] * rightCellValues[0] );
+  RightFluxes_V += (.5 * g * Normals[1] ) * ( rightCellValues[0] * rightCellValues[0] );
   //end of inlined
 
 
   out[0] =
     ( t1 * LeftFluxes_H ) +
     ( t2 * RightFluxes_H ) +
-    ( t3 * ( RightFacetValues[0] - LeftFacetValues[0] ) );
+    ( t3 * ( rightCellValues[0] - leftCellValues[0] ) );
 
   out[1] =
     ( t1 * LeftFluxes_U ) +
     ( t2 * RightFluxes_U ) +
-    ( t3 * ( (RightFacetValues[0] * RightFacetValues[1]) -
-    (LeftFacetValues[0] * LeftFacetValues[1]) ) );
+    ( t3 * ( (rightCellValues[0] * rightCellValues[1]) -
+    (leftCellValues[0] * leftCellValues[1]) ) );
 
   out[2] =
     ( t1 * LeftFluxes_V ) +
     ( t2 * RightFluxes_V ) +
-    ( t3 * ( (RightFacetValues[0] * RightFacetValues[2]) -
-    (LeftFacetValues[0] * LeftFacetValues[2]) ) );
+    ( t3 * ( (rightCellValues[0] * rightCellValues[2]) -
+    (leftCellValues[0] * leftCellValues[2]) ) );
 
   out[0] *= *FacetVolumes;
   out[1] *= *FacetVolumes;
