a82c75afb439a1b674c5916006ff33e597352702
Istvan Reguly <regulyistvan@gmail.com>
2012-10-12 14:46:24 +0100

    one iteration runs

--- apps/c/volna/NumericalFluxes_1.h
+++ apps/c/volna/NumericalFluxes_1.h
@@ -7,26 +7,26 @@ void NumericalFluxes_1(float *leftCellValues, //OP_READ
 )
 {
   float cL = sqrt(g * leftCellValues[0]);
-  cL = cL > 0.0 ? cL : 0.0;
+  cL = cL > 0.0f ? cL : 0.0f;
   float cR = sqrt(g * rightCellValues[0]);
-  cR = cR > 0.0 ? cR : 0.0;
+  cR = cR > 0.0f ? cR : 0.0f;
 
   float uLn = leftCellValues[1] * Normals[0] + leftCellValues[2] * Normals[1];
   float uRn = rightCellValues[1] * Normals[0] + rightCellValues[2] * Normals[1];
 
-  float unStar = 0.5 * (uLn + uRn) - 0.25* (cL+cR);
-  float cStar = 0.5 * (cL + cR) - 0.25* (uLn-uRn);
+  float unStar = 0.5f * (uLn + uRn) - 0.25f* (cL+cR);
+  float cStar = 0.5f * (cL + cR) - 0.25f* (uLn-uRn);
 
   float sL = (uLn - cL) < (unStar - cStar) ? (uLn - cL) : (unStar - cStar);
-  float sLMinus = sL < 0.0 ? sL : 0.0;
+  float sLMinus = sL < 0.0f ? sL : 0.0f;
 
   float sR = (uRn + cR) > (unStar + cStar) ? (uRn + cR) : (unStar + cStar);
-  float sRPlus = sR > 0.0 ? sR : 0.0;
+  float sRPlus = sR > 0.0f ? sR : 0.0f;
 
-  sL = leftCellValues[0] < EPS ? uRn - 2.0*cR : sL; // is this 2.0 or 2? (i.e. float/int)
+  sL = leftCellValues[0] < EPS ? uRn - 2.0f*cR : sL; // is this 2.0f or 2? (i.e. float/int)
   sR = leftCellValues[0] < EPS ? uRn + cR : sR;
 
-  sR = rightCellValues[0] < EPS ? uLn + 2.0*cL : sR; // is this 2.0 or 2? (i.e. float/int)
+  sR = rightCellValues[0] < EPS ? uLn + 2.0f*cL : sR; // is this 2.0f or 2? (i.e. float/int)
   sL = rightCellValues[0] < EPS ? uLn - cL : sL;
 
   float sRMinussL = sRPlus - sLMinus;
@@ -35,7 +35,7 @@ void NumericalFluxes_1(float *leftCellValues, //OP_READ
   float t1 = sRPlus / sRMinussL;
   //assert( ( 0 <= t1 ) && ( t1 <= 1 ) );
 
-  float t2 = ( -1.0 * sLMinus ) / sRMinussL;
+  float t2 = ( -1.0f * sLMinus ) / sRMinussL;
   //assert( ( 0 <= t2 ) && ( t2 <= 1 ) );
 
   float t3 = ( sRPlus * sLMinus ) / sRMinussL;
@@ -49,8 +49,8 @@ void NumericalFluxes_1(float *leftCellValues, //OP_READ
   LeftFluxes_U = HuDotN * leftCellValues[1];
   LeftFluxes_V = HuDotN * leftCellValues[2];
 
-  LeftFluxes_U += (.5 * g * Normals[0] ) * ( leftCellValues[0] * leftCellValues[0] );
-  LeftFluxes_V += (.5 * g * Normals[1] ) * ( leftCellValues[0] * leftCellValues[0] );
+  LeftFluxes_U += (.5f * g * Normals[0] ) * ( leftCellValues[0] * leftCellValues[0] );
+  LeftFluxes_V += (.5f * g * Normals[1] ) * ( leftCellValues[0] * leftCellValues[0] );
   //end of inlined
 
   float RightFluxes_H, RightFluxes_U, RightFluxes_V;
@@ -62,8 +62,8 @@ void NumericalFluxes_1(float *leftCellValues, //OP_READ
   RightFluxes_U =   HuDotN * rightCellValues[1];
   RightFluxes_V =   HuDotN * rightCellValues[2];
 
-  RightFluxes_U += (.5 * g * Normals[0] ) * ( rightCellValues[0] * rightCellValues[0] );
-  RightFluxes_V += (.5 * g * Normals[1] ) * ( rightCellValues[0] * rightCellValues[0] );
+  RightFluxes_U += (.5f * g * Normals[0] ) * ( rightCellValues[0] * rightCellValues[0] );
+  RightFluxes_V += (.5f * g * Normals[1] ) * ( rightCellValues[0] * rightCellValues[0] );
   //end of inlined
 
 
@@ -87,11 +87,12 @@ void NumericalFluxes_1(float *leftCellValues, //OP_READ
   out[0] *= *FacetVolumes;
   out[1] *= *FacetVolumes;
   out[2] *= *FacetVolumes;
-  out[3] = 0.0;
+  out[3] = 0.0f;
 
-  float maximum = abs(uLn + cL);
-  maximum = maximum > abs(uLn - cL) ? maximum : abs(uLn - cL);
-  maximum = maximum > abs(uRn + cR) ? maximum : abs(uRn + cR);
-  maximum = maximum > abs(uRn - cR) ? maximum : abs(uRn - cR);
+  float maximum = fabs(uLn + cL);
+  maximum = maximum > fabs(uLn - cL) ? maximum : fabs(uLn - cL);
+  maximum = maximum > fabs(uRn + cR) ? maximum : fabs(uRn + cR);
+  maximum = maximum > fabs(uRn - cR) ? maximum : fabs(uRn - cR);
   *maxFacetEigenvalues = maximum;
+
 }
