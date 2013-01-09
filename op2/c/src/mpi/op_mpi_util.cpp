39200044d0afa960bc6bf4329aac3f911d0b927a
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-01-09 12:51:51 +0000

    Simplify fetch_data_hdf5 by templating over different data types

---
+++ op2/c/src/mpi/op_mpi_util.cpp
@@ -0,0 +1,51 @@
+/*
+ * Open source copyright declaration based on BSD open source template:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * This file is part of the OP2 distribution.
+ *
+ * Copyright (c) 2011, Mike Giles and others. Please see the AUTHORS file in
+ * the main source directory for a full list of copyright holders.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of Mike Giles may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Mike Giles ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Mike Giles BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <op_lib_core.h>
+#include <op_mpi_util.h>
+
+/*******************************************************************************
+* Routine to fetch data from an op_dat to user allocated memory block under hdf5
+* -- placed in op_mpi_core.c as this routine does not use any hdf5 functions
+*******************************************************************************/
+
+void fetch_data_hdf5(op_dat dat, char* usr_ptr, int low, int high)
+{
+  if(strcmp(dat->type,"double") == 0)
+    gather_data_hdf5<double>(dat, usr_ptr, low, high);
+  else if(strcmp(dat->type,"float") == 0)
+    gather_data_hdf5<float>(dat, usr_ptr, low, high);
+  else if(strcmp(dat->type,"int") == 0)
+    gather_data_hdf5<int>(dat, usr_ptr, low, high);
+  else
+    printf("Unknown type %s, cannot error in fetch_data_hdf5() \n",dat->type);
+}
