bf2e140eb46f9edc52fef1b2622c43f3eff9dd99
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-10-29 18:18:40 +0000

    Fix cuda __aligne__ error when compiling with pgi compilers

--- cmake/modules/Compiler/PGI-C.cmake
+++ cmake/modules/Compiler/PGI-C.cmake
@@ -1,7 +1,11 @@
 message(STATUS "Setting PGI C compiler options")
 include(Compiler/PGI)
 __compiler_pgi(C)
-SET(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -c99")
+
+#cuda required a compatible definition for __align__
+add_definitions( -DCUDARTAPI= -D__align__\\\(n\\\)=__attribute__\\\(\\\(aligned\\\(n\\\)\\\)\\\) -D__location__\\\(a\\\)=__annotate__\\\(a\\\) )
+
+SET(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -c99 ")
 set(CMAKE_C_FLAGS_MINSIZEREL_INIT "${CMAKE_C_FLAGS_MINSIZEREL_INIT} -DNDEBUG")
 set(CMAKE_C_FLAGS_RELEASE_INIT "${CMAKE_C_FLAGS_RELEASE_INIT} -fastsse -Minline=levels:10 -Mvect -Minfo -DNDEBUG")
 
