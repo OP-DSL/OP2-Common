9a50b39d547d8b90393031e770f7074279ed10c2
Gihan R. Mudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-12-06 09:47:21 +0000

    Updated user guide

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -3,6 +3,8 @@ pdflatex C++_Users_Guide.tex
 pdflatex C++_Users_Guide.tex
 pdflatex dev.tex
 pdflatex dev.tex
+pdflatex mpi-dev.tex
+pdflatex mpi-dev.tex
 pdflatex --shell-escape airfoil-doc.tex
 pdflatex --shell-escape airfoil-doc.tex
 rm -f *.out *.bbl *.aux *.blg *.pyg.* *.log *.backup *.toc *~
