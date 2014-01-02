5646a8350dc057d18bf20c568f00987f41af107d
Gihan R. Mudalige <gihan.mudalige@oerc.ox.ac.uk>
2014-01-02 11:23:51 +0000

    Removing some .sty files in order to document them being dependant on debian pakages

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+#set -e
+#check/install pakages
+#sudo apt-get install texlive-science  python-pygments latex-xcolor
+
 pdflatex C++_Users_Guide.tex
 pdflatex C++_Users_Guide.tex
 pdflatex dev.tex
 pdflatex dev.tex
+
 pdflatex --shell-escape mpi-dev.tex
 pdflatex --shell-escape mpi-dev.tex
+bibtex mpi-dev
+pdflatex --shell-escape mpi-dev.tex
+
+pdflatex --shell-escape airfoil-doc.tex
+bibtex airfoil-doc
 pdflatex --shell-escape airfoil-doc.tex
 pdflatex --shell-escape airfoil-doc.tex
-rm -f *.out *.bbl *.aux *.blg *.pyg.* *.log *.backup *.toc *~
+
+rm -f *.out *.aux *.blg *.pyg.* *.log *.backup *.toc *~ *.bbl
 
