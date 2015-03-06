70865212424f0634b712a6b1400c35d4ee47da2b
reguly <regulyistvan@gmail.com>
2015-03-06 17:53:19 +0000

    Multi-file fix

--- translator/c/python/op2.py
+++ translator/c/python/op2.py
@@ -41,8 +41,8 @@ from op2_gen_mpi_vec import op2_gen_mpi_vec
 
 # import OpenMP and CUDA code generation functions
 from op2_gen_openmp_simple import op2_gen_openmp_simple
-
-#from op2_gen_cuda import op2_gen_cuda
+from op2_gen_seq import op2_gen_seq
+from op2_gen_cuda import op2_gen_cuda
 from op2_gen_cuda_simple import op2_gen_cuda_simple
 from op2_gen_cuda_simple_hyb import op2_gen_cuda_simple_hyb
 
@@ -630,7 +630,10 @@ def main():
                 fid.write(' "op_lib_cpp.h"\n\n')
                 line = '\n#define STRIDE(x,y) x\n'
                 for ns in range (0,len(sets)):
-                  line += 'int '+sets[ns]['name'].replace('"','')+'_stride = 1;\n'
+                  if a == 1:
+                    line += 'int '+sets[ns]['name'].replace('"','')+'_stride = 1;\n'
+                  else:
+                    line += 'extern int '+sets[ns]['name'].replace('"','')+'_stride;\n'
                 fid.write(line)
                 fid.write('//\n// op_par_loop declarations\n//\n')
                 for k_iter in range(0, len(kernels_in_files[a - 1])):
