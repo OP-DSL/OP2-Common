208239c3a16cc631e056a08a454e0576ba34e7e7
Istvan Reguly <regulyistvan@gmail.com>
2017-04-07 21:04:03 +0100

    regenerated, better typing of setKernelTime

--- apps/fortran/airfoil/airfoil_hdf5/dp/save_soln_seqkernel.F90
+++ apps/fortran/airfoil/airfoil_hdf5/dp/save_soln_seqkernel.F90
@@ -69,7 +69,7 @@ SUBROUTINE save_soln_host( userSubroutine, set, &
   opArgArray(2) = opArg2
 
   returnSetKernelTiming = setKernelTime(0 , userSubroutine//C_NULL_CHAR, &
-  & 0.d0, 0.00000_4,0.00000_4, 0)
+  & 0.0_8, 0.00000_4,0.00000_4, 0)
   call op_timers_core(startTime)
 
   n_upper = op_mpi_halo_exchanges(set%setCPtr,numberOfOpDats,opArgArray)
