595233756e6ea7432a66dacb678dba9e6079970b
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2017-05-06 15:51:12 +0100

    Test script updated

--- scripts/test_makefiles.sh
+++ scripts/test_makefiles.sh
@@ -30,7 +30,7 @@ cd ../../fortran/python/
 export OP2_FORT_CODEGEN_DIR=$PWD
 cd $OP2_INSTALL_PATH/c
 
-<<COMMENT0
+#<<COMMENT0
 
 echo " "
 echo " "
@@ -341,7 +341,7 @@ export OMP_NUM_THREADS=2
 validate "$MPI_INSTALL_PATH/bin/mpirun -np 10 ./airfoil_hdf5_mpi OP_MAPS_BASE_INDEX=0"
 #_openmp_$PART_SIZE_ENV
 
-COMMENT0
+#COMMENT0
 
 ###################################################################################
 ###################################################################################
