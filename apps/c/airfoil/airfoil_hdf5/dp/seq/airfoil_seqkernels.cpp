86985e400abbc8e45b5acc61d4b9da815e4c4e5e
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2018-01-21 17:15:41 +0000

    Moving JIT application to a different directory

--- apps/c/airfoil/airfoil_hdf5/dp/seq/airfoil_seqkernels.cpp
+++ apps/c/airfoil/airfoil_hdf5/dp/seq/airfoil_seqkernels.cpp
@@ -10,42 +10,9 @@ extern double eps;
 extern double mach;
 extern double alpha;
 extern double qinf[4];
-
-<<<<<<< HEAD
-// header
-#include "op_lib_cpp.h"
-
-=======
->>>>>>> Working bres_calc, adt_calc and update
 // user kernel files
 #include "save_soln_seqkernel.cpp"
 #include "adt_calc_seqkernel.cpp"
 #include "res_calc_seqkernel.cpp"
 #include "bres_calc_seqkernel.cpp"
 #include "update_seqkernel.cpp"
-
-#ifdef OPS_JIT
-void jit_consts() {
-  FILE *f = fopen("jit_const.h", "r");
-    if (f == NULL) {
-      f = fopen("jit_const.h", "w"); // create only if file does not exist
-      if (f == NULL) {
-        printf("Error opening file!\n");
-        exit(1);
-      }
-      /*need to generate this block of code using the code generator
-      using what is declared in op_decal_consts
-      */
-      fprintf(f, "#define gam %lf\n", gam);
-      fprintf(f, "#define gm1 %lf\n", gm1);
-      fprintf(f, "#define cfl %lf\n", cfl);
-      fprintf(f, "#define eps %lf\n", eps);
-      fprintf(f, "#define mach %lf\n", mach);
-      fprintf(f, "#define alpha %lf\n", alpha);
-
-      fprintf(f, "extern double qinf[4];\n");
-
-      fclose(f);
-    }
-}
-#endif
