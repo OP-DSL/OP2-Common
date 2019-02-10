df2ac35e7f88d0fb7f4ce46ec83d5cd8e952807d
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2019-02-10 14:06:32 +0000

    Fixing source file locations for tests on Octon

--- apps/c/aero/aero_plain/dp/openmp4/dirichlet_omp4kernel_func.cpp
+++ apps/c/aero/aero_plain/dp/openmp4/dirichlet_omp4kernel_func.cpp
@@ -6,10 +6,9 @@ void dirichlet_omp4_kernel(int *map0, int map0size, double *data0, int dat0size,
                            int *col_reord, int set_size1, int start, int end,
                            int num_teams, int nthread) {
 
-#pragma omp target teams distribute parallel for schedule(                     \
-    static, 1) num_teams(num_teams) thread_limit(nthread)                      \
-        map(to : col_reord[0 : set_size1], map0[0 : map0size],                 \
-                                                data0[0 : dat0size])
+#pragma omp target teams num_teams(num_teams) thread_limit(nthread) map(       \
+    to : col_reord[0 : set_size1], map0[0 : map0size], data0[0 : dat0size])
+#pragma omp distribute parallel for schedule(static, 1)
   for ( int e=start; e<end; e++ ){
     int n_op = col_reord[e];
     int map0idx = map0[n_op + set_size1 * 0];
