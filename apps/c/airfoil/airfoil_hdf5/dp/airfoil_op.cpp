4ce646659834483d208a35fdfa908fa03cfb2cbf
sikba <siklosi.balint@hallgato.ppke.hu>
2019-04-11 16:55:57 +0200

    more cleanup

--- apps/c/airfoil/airfoil_hdf5/dp/airfoil_op.cpp
+++ apps/c/airfoil/airfoil_hdf5/dp/airfoil_op.cpp
@@ -206,10 +206,8 @@ int main(int argc, char **argv) {
   op_write_const_hdf5("qinf", 4, "double", (char *)qinf, "new_grid_out.h5");
 
   // trigger partitioning and halo creation routines
-  //op_partition("PTSCOTCH", "KWAY", edges, pecell, p_x);
-
-
-  op_partition("PARMETIS", "KWAY", edges, pecell, p_x);
+  op_partition("PTSCOTCH", "KWAY", edges, pecell, p_x);
+  // op_partition("PARMETIS", "KWAY", edges, pecell, p_x);
   if (renumber) op_renumber(pecell);
 
 #define PDIM 2
@@ -222,35 +220,21 @@ int main(int argc, char **argv) {
 
   niter = 1000;
 
-  char debug_msg[200];
-
-
-// do stuff with msg
-
-
-
   for (int iter = 1; iter <= niter; iter++) {
 
     //  save old flow solution
-    snprintf(debug_msg, 200, "kernel_name: %s, iteration: %d/%d\n", "save_soln", iter,niter);
+
     op_par_loop_save_soln("save_soln",cells,
                 op_arg_dat(p_q,-1,OP_ID,4,"double",OP_READ),
                 op_arg_dat(p_qold,-1,OP_ID,4,"double",OP_WRITE));
 
-
-
-
-
-	if (iter % 10 == 0 )
-		op_printf("Calculating res_calc %d/%d\n",iter,niter);
-
     //  predictor/corrector update loop
+
     for (int k = 0; k < 2; k++) {
 
       //    calculate area/timstep
 
-      //snprintf(debug_msg, 200, "kernel_name: %s, iteration: %d/%d, k: %d\n", "adt_calc", iter,niter,k);
-      op_par_loop_adt_calc( "adt_calc",cells,
+      op_par_loop_adt_calc("adt_calc",cells,
                   op_arg_dat(p_x,0,pcell,2,"double",OP_READ),
                   op_arg_dat(p_x,1,pcell,2,"double",OP_READ),
                   op_arg_dat(p_x,2,pcell,2,"double",OP_READ),
@@ -259,10 +243,6 @@ int main(int argc, char **argv) {
                   op_arg_dat(p_adt,-1,OP_ID,1,"double",OP_WRITE));
 
       //    calculate flux residual
-
-      //snprintf(debug_msg, 200, "before: %s, iteration: %d/%d, k: %d\n", "res_calc", iter,niter,k);
-      //comapre_mpi_halo_check(p_res,debug_msg);
-      //snprintf(debug_msg, 200, "kernel_name: %s, iteration: %d/%d, k: %d\n", "res_calc", iter,niter,k);
 
       op_par_loop_res_calc("res_calc",edges,
                   op_arg_dat(p_x,0,pedge,2,"double",OP_READ),
@@ -274,17 +254,6 @@ int main(int argc, char **argv) {
                   op_arg_dat(p_res,0,pecell,4,"double",OP_INC),
                   op_arg_dat(p_res,1,pecell,4,"double",OP_INC));
 
-      if (iter == 1 && k == 0) {
-        snprintf(debug_msg, 200, "after: %s, iteration: %d/%d, k: %d\n", "res_calc and forced exchange", iter,niter,k);
-        //forced_exchange(p_res);
-        comapre_mpi_halo_check(p_res,debug_msg);
-        //op_fetch_data_hdf5_file(p_res, "res.h5");
-        exit(-1);
-      }
-
-
-
-      //snprintf(debug_msg, 200, "kernel_name: %s, iteration: %d/%d, k: %d\n", "bres_calc", iter,niter,k);
       op_par_loop_bres_calc("bres_calc",bedges,
                   op_arg_dat(p_x,0,pbedge,2,"double",OP_READ),
                   op_arg_dat(p_x,1,pbedge,2,"double",OP_READ),
@@ -297,8 +266,6 @@ int main(int argc, char **argv) {
 
       rms = 0.0;
 
-      //snprintf(debug_msg, 200, "kernel_name: %s, iteration: %d/%d, k: %d\n", "update", iter,niter,k);
-      //op_par_loop_update("update",cells,
       op_par_loop_update("update",cells,
                   op_arg_dat(p_qold,-1,OP_ID,4,"double",OP_READ),
                   op_arg_dat(p_q,-1,OP_ID,4,"double",OP_WRITE),
@@ -330,10 +297,6 @@ int main(int argc, char **argv) {
   }
 
   op_timers(&cpu_t2, &wall_t2);
-
-    // output the result dat array to files
- // op_print_dat_to_txtfile(p_q, "out_grid_seq.dat"); // ASCI
-  op_print_dat_to_binfile(p_q, "out_grid_seq_reversed.bin"); // Binary
 
   // write given op_dat's indicated segment of data to a memory block in the
   // order it was originally
