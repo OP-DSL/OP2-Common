35dde31859f994e2394831b1abce52bcd903692a
Gihan Mudalige (RR) <gihan@kos.hpsg.dcs.warwick.ac.uk>
2019-07-27 11:04:53 +0100

    Updating hipsycl source

--- translator/c/python/op2_gen_sycl.py
+++ translator/c/python/op2_gen_sycl.py
@@ -106,10 +106,10 @@ def op2_gen_sycl(master, date, consts, kernels,sets, macro_defs):
 
   accsstring = ['OP_READ','OP_WRITE','OP_RW','OP_INC','OP_MAX','OP_MIN' ]
 
-  inc_stage=1
+  inc_stage=0
   op_color2=0
   op_color2_force=0
-  atomics=0
+  atomics=1
 ##########################################################################
 #  create new kernel file
 ##########################################################################
