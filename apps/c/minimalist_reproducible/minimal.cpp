27a6f2001d63670f70517af2c855a0d96e922431
Istvan Reguly <regulyistvan@gmail.com>
2019-12-20 14:05:01 +0100

    Dump file & read it

--- apps/c/minimalist_reproducible/minimal.cpp
+++ apps/c/minimalist_reproducible/minimal.cpp
@@ -47,72 +47,16 @@ int main(int argc, char **argv) {
 
   // timer
   double cpu_t1, cpu_t2, wall_t1, wall_t2;
-
-  int nnode, nedge, n, e;
-
-  nnode = (NN - 1) * (NN - 1);
-  nedge = (NN - 1) * (NN - 1) + 4 * (NN - 1) * (NN - 2);
-
-  int *pp = (int *)malloc(sizeof(int) * 2 * nedge);
-
-  double *A = (double *)malloc(sizeof(double) * nedge);
-  double *r = (double *)malloc(sizeof(double) * nnode);
-
-  // create matrix and r.h.s., and set coordinates needed for renumbering /
-  // partitioning
-
-  e = 0;
-
-  for (int i = 1; i < NN; i++) {
-    for (int j = 1; j < NN; j++) {
-      n = i - 1 + (j - 1) * (NN - 1);
-      pp[2 * e] = n;
-      pp[2 * e + 1] = n;
-      e++;
-
-      for (int pass = 0; pass < 4; pass++) {
-        int i2 = i;
-        int j2 = j;
-        if (pass == 0)
-          i2 += -1;
-        if (pass == 1)
-          i2 += 1;
-        if (pass == 2)
-          j2 += -1;
-        if (pass == 3)
-          j2 += 1;
-
-        if ((i2 == 0) || (i2 == NN) || (j2 == 0) || (j2 == NN)) {
-          ;
-        } else {
-          pp[2 * e] = n;
-          pp[2 * e + 1] = i2 - 1 + (j2 - 1) * (NN - 1);
-          e++;
-        }
-      }
-    }
-  }
-  
-  for (int i = 0; i < nnode; i++) {
-    r[i]=3;
-  }
-  
-  
-  for (int i = 0; i < nedge; i++) {
-    A[i]=i+2;
-  }
-  
-  
-
+  const char* file = "data.h5";
   // declare sets, pointers, and datasets
 
-  op_set nodes = op_decl_set(nnode, "nodes");
-  op_set edges = op_decl_set(nedge, "edges");
+  op_set nodes = op_decl_set_hdf5(file, "nodes");
+  op_set edges = op_decl_set_hdf5(file, "edges");
 
-  op_map ppedge = op_decl_map(edges, nodes, 2, pp, "ppedge");
+  op_map ppedge = op_decl_map_hdf5(edges, nodes, 2, file, "ppedge");
 
-  op_dat p_A = op_decl_dat(edges, 1, "double", A, "p_A");
-  op_dat p_r = op_decl_dat(nodes, 1, "double", r, "p_r");
+  op_dat p_A = op_decl_dat_hdf5(edges, 1, "double", file, "p_A");
+  op_dat p_r = op_decl_dat_hdf5(nodes, 1, "double", file, "p_r");
 
   alpha = 1.0f;
   op_decl_const(1, "double", &alpha);
