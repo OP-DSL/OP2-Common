2251c0f52ae1af6266a46684e827d2e12a36546e
Gihan Mudalige (RR) <gihan@kos.hpsg.dcs.warwick.ac.uk>
2020-01-29 12:17:04 +0000

    Fix hydra reduce_double issue due to type "real*8" and "r8" strings

--- translator/fortran/python/op2_gen_mpiseq3.py
+++ translator/fortran/python/op2_gen_mpiseq3.py
@@ -552,11 +552,11 @@ def op2_gen_mpiseq3(master, date, consts, kernels, hydra, bookleaf):
     #reductions
     for g_m in range(0,nargs):
       if maps[g_m] == OP_GBL and (accs[g_m] == OP_INC or accs[g_m] == OP_MIN or accs[g_m] == OP_MAX or accs[g_m] == OP_WRITE):
-        if typs[g_m] == 'real(8)' or typs[g_m] == 'REAL(kind=8)':
+        if typs[g_m] == 'real(8)' or typs[g_m] == 'REAL(kind=8)' or typs[g_m] == 'real*8' or typs[g_m] == 'r8':
           code('CALL op_mpi_reduce_double(opArg'+str(g_m+1)+',opArg'+str(g_m+1)+'%data)')
-        elif typs[g_m] == 'real(4)' or typs[g_m] == 'REAL(kind=4)':
+        elif typs[g_m] == 'real(4)' or typs[g_m] == 'REAL(kind=4)' or typs[g_m] == 'real*4' or typs[g_m] == 'r4':
           code('CALL op_mpi_reduce_float(opArg'+str(g_m+1)+',opArg'+str(g_m+1)+'%data)')
-        elif typs[g_m] == 'integer(4)' or typs[g_m] == 'INTEGER(kind=4)':
+        elif typs[g_m] == 'integer(4)' or typs[g_m] == 'INTEGER(kind=4)' or typs[g_m] == 'integer*4' or typs[g_m] == 'i4':
           code('CALL op_mpi_reduce_int(opArg'+str(g_m+1)+',opArg'+str(g_m+1)+'%data)')
         elif typs[g_m] == 'logical' or typs[g_m] == 'logical*1':
           code('CALL op_mpi_reduce_bool(opArg'+str(g_m+1)+',opArg'+str(g_m+1)+'%data)')
