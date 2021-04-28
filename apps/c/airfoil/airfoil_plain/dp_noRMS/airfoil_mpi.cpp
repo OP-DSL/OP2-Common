fef5cf05aebc61a2d5560959e7b2b07fc23482ef
Suneth Ekanayake <ekanayake.suneth@gmail.com>
2021-04-28 10:01:29 +0000

    Makefile and airfoil_mpi.cpp changes

--- apps/c/airfoil/airfoil_plain/dp_noRMS/airfoil_mpi.cpp
+++ apps/c/airfoil/airfoil_plain/dp_noRMS/airfoil_mpi.cpp
@@ -415,10 +415,10 @@ int main(int argc, char **argv) {
 
   // print iteration history
   rms = sqrt(rms / (double)g_ncell);
-  if (iter % 100 == 0)
-    op_printf(" %d  %10.5e \n", iter, rms);
+  // if (iter % 100 == 0)
+  //   op_printf(" %d  %10.5e \n", iter, rms);
 
-  if (iter % 1000 == 0 &&
+  if (niter % 1000 == 0 &&
       g_ncell == 720000) { // defailt mesh -- for validation testing
     // op_printf(" %d  %3.16f \n",iter,rms);
     double diff = fabs((100.0 * (rms / 0.0001060114637578)) - 100.0);
