2825770f6bf43a79228f4ff0fcb8ce4ec8dd0eba
Joe Kaushal <joe.kaushal@gmail.com>
2021-10-21 09:52:44 +0100

    Remove un-maintained CMake build system

--- cmake/modules/Compiler/Intel-Fortran.cmake
+++
@@ -1,8 +0,0 @@
-message(STATUS "Setting Intel Fortran compiler options")
-
-SET(CMAKE_Fortran_FLAGS_DIAGS_INIT "-warn all -warn error")
-
-include(Compiler/Intel)
-__compiler_intel(Fortran)
-
-SET(CMAKE_Fortran_MODDIR_FLAG "-module ")
