efaacd43ea06839b36c34a270062ae118dd057e7
Joe Kaushal <joe.kaushal@gmail.com>
2021-10-28 11:32:23 +0100

    Finer-grained wiring of include path

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -8,14 +8,16 @@ MAKEFILES_DIR := $(shell dirname $(realpath \
 ROOT_DIR := $(shell realpath $(MAKEFILES_DIR)/../)
 OP2_BUILD_DIR ?= $(ROOT_DIR)/op2
 
+OP2_INC ?= -I$(ROOT_DIR)/op2/include
+OP2_LIB ?= -L$(OP2_BUILD_DIR)/lib
+
 AR = ar rcs
-INC += -Iinclude
 
 ifdef CUDA_INSTALL_PATH
   include $(MAKEFILES_DIR)/nvcc.mk
 
-  INC += -I$(CUDA_INSTALL_PATH)/include
-  LIB += -lcudart
+  CUDA_INC ?= -I$(CUDA_INSTALL_PATH)/include
+  CUDA_LIB ?= -lcudart
 endif
 
 ifdef MPI_INSTALL_PATH
@@ -23,23 +25,23 @@ ifdef MPI_INSTALL_PATH
   MPICXX ?= $(MPI_INSTALL_PATH)/bin/mpic++
   MPIFC ?= $(MPI_INSTALL_PATH)/bin/mpifort
 
-  INC += -I$(MPI_INSTALL_PATH)/include
-  LIB += -L$(MPI_INSTALL_PATH)/lib -lmpi
+  MPI_INC ?= -I$(MPI_INSTALL_PATH)/include
+  MPI_LIB ?= -L$(MPI_INSTALL_PATH)/lib -lmpi
 endif
 
 ifdef PARMETIS_INSTALL_PATH
-  INC += -I$(PARMETIS_INSTALL_PATH)/include -DHAVE_PARMETIS -DPARMETIS_VER_4
-  LIB += -L$(PARMETIS_INSTALL_PATH)/lib -lparmetis -lmetis
+  PARMETIS_INC ?= -I$(PARMETIS_INSTALL_PATH)/include -DHAVE_PARMETIS -DPARMETIS_VER_4
+  PARMETIS_LIB ?= -L$(PARMETIS_INSTALL_PATH)/lib -lparmetis -lmetis
 endif
 
 ifdef PTSCOTCH_INSTALL_PATH
-  INC += -I$(PTSCOTCH_INSTALL_PATH)/include -DHAVE_PTSCOTCH
-  LIB += -L$(PTSCOTCH_INSTALL_PATH)/lib -lscotch -lptscotch -lptscotcherr
+  PTSCOTCH_INC += -I$(PTSCOTCH_INSTALL_PATH)/include -DHAVE_PTSCOTCH
+  PTSCOTCH_LIB += -L$(PTSCOTCH_INSTALL_PATH)/lib -lscotch -lptscotch -lptscotcherr
 endif
 
 ifdef HDF5_INSTALL_PATH
-  INC += -I$(HDF5_INSTALL_PATH)/include
-  LIB += -I$(HDF5_INSTALL_PATH)/lib -lhdf5 -lz -ldl -lm
+  HDF5_INC += -I$(HDF5_INSTALL_PATH)/include
+  HDF5_LIB += -L$(HDF5_INSTALL_PATH)/lib -l:libhdf5.a -ldl -lm
 endif
 
 include $(MAKEFILES_DIR)/compilers/$(OP2_COMPILER).mk
