27d144622e3c1dc71e71062e5c68608fd5e9f043
Joe Kaushal <joe.kaushal@gmail.com>
2021-10-29 16:21:39 +0100

    (Re-)Port jac2.cpp to MPI

--- apps/c/jac2/jac_mpi.cpp
+++ apps/c/jac2/jac_mpi.cpp
@@ -47,15 +47,9 @@
 // mpi header file - included by user for user level mpi
 //
 
-#include <mpi.h>
-
 // global constants
 
 float alpha;
-
-// jac header file
-
-#include "check_result.h"
 
 //
 // OP header file
@@ -63,6 +57,11 @@ float alpha;
 
 #include "op_lib_mpi.h"
 #include "op_seq.h"
+
+// jac header file
+
+#define STRIDE 2
+#include "check_result.h"
 
 //
 // kernel routines for parallel loops
@@ -73,7 +72,7 @@ float alpha;
 
 // Error tolerance in checking correctness
 
-#define TOLERANCE 1e-12
+#define TOLERANCE 1e-6
 
 //
 // user declared functions
@@ -111,8 +110,9 @@ static void scatter_float_array(float *g_array, float *l_array, int comm_size,
   free(displs);
 }
 
-void scatter_double_array(double *g_array, double *l_array, int comm_size,
-                          int g_size, int l_size, int elem_size) {
+static void scatter_double_array(double *g_array, double *l_array,
+                                 int comm_size, int g_size, int l_size,
+                                 int elem_size) {
   int *sendcnts = (int *)malloc(comm_size * sizeof(int));
   int *displs = (int *)malloc(comm_size * sizeof(int));
   int disp = 0;
@@ -173,32 +173,27 @@ int main(int argc, char **argv) {
   // timer
   double cpu_t1, cpu_t2, wall_t1, wall_t2;
 
-  int *pp;
-  float *r, *u, *du;
-  double *A;
-
-  int nnode, nedge;
-
   /**------------------------BEGIN I/O and PARTITIONING ---------------------**/
-
   int g_nnode, g_nedge, g_n, g_e;
 
   g_nnode = (NN - 1) * (NN - 1);
   g_nedge = (NN - 1) * (NN - 1) + 4 * (NN - 1) * (NN - 2);
 
-  int *g_pp = 0;
-  float *g_r = 0, *g_u = 0, *g_du = 0;
-  double *g_A = 0;
+  int *g_pp = nullptr, *g_p1 = nullptr, *g_p2 = nullptr;
+  double *g_A = nullptr;
+  float *g_r = nullptr, *g_u = nullptr, *g_du = nullptr;
 
   op_printf("Global number of nodes, edges = %d, %d\n", g_nnode, g_nedge);
 
   if (my_rank == MPI_ROOT) {
     g_pp = (int *)malloc(sizeof(int) * 2 * g_nedge);
+    g_p1 = (int *)malloc(sizeof(int) * g_nedge);
+    g_p2 = (int *)malloc(sizeof(int) * g_nedge);
 
-    g_A = (double *)malloc(sizeof(double) * g_nedge);
-    g_r = (float *)malloc(sizeof(float) * g_nnode);
-    g_u = (float *)malloc(sizeof(float) * g_nnode);
-    g_du = (float *)malloc(sizeof(float) * g_nnode);
+    g_A = (double *)malloc(sizeof(double) * 3 * g_nedge);
+    g_r = (float *)malloc(sizeof(float) * 2 * g_nnode);
+    g_u = (float *)malloc(sizeof(float) * 2 * g_nnode);
+    g_du = (float *)malloc(sizeof(float) * 3 * g_nnode);
 
     // create matrix and r.h.s., and set coordinates needed for renumbering /
     // partitioning
@@ -208,13 +203,15 @@ int main(int argc, char **argv) {
     for (int i = 1; i < NN; i++) {
       for (int j = 1; j < NN; j++) {
         g_n = i - 1 + (j - 1) * (NN - 1);
-        g_r[g_n] = 0.0f;
-        g_u[g_n] = 0.0f;
-        g_du[g_n] = 0.0f;
+        g_r[2 * g_n] = 0.0f;
+        g_u[2 * g_n] = 0.0f;
+        g_du[3 * g_n] = 0.0f;
 
-        g_pp[2 * g_e] = g_n;
-        g_pp[2 * g_e + 1] = g_n;
-        g_A[g_e] = -1.0f;
+        g_p1[g_e] = g_n;
+        g_p2[g_e] = g_n;
+        g_pp[2 * g_e] = g_p1[g_e];
+        g_pp[2 * g_e + 1] = g_p2[g_e];
+        g_A[3 * g_e] = -1.0f;
         g_e++;
 
         for (int pass = 0; pass < 4; pass++) {
@@ -230,11 +227,13 @@ int main(int argc, char **argv) {
             j2 += 1;
 
           if ((i2 == 0) || (i2 == NN) || (j2 == 0) || (j2 == NN)) {
-            g_r[g_n] += 0.25f;
+            g_r[2 * g_n] += 0.25f;
           } else {
-            g_pp[2 * g_e] = g_n;
-            g_pp[2 * g_e + 1] = i2 - 1 + (j2 - 1) * (NN - 1);
-            g_A[g_e] = 0.25f;
+            g_p1[g_e] = g_n;
+            g_p2[g_e] = i2 - 1 + (j2 - 1) * (NN - 1);
+            g_pp[2 * g_e] = g_p1[g_e];
+            g_pp[2 * g_e + 1] = g_p2[g_e];
+            g_A[3 * g_e] = 0.25f;
             g_e++;
           }
         }
@@ -243,30 +242,33 @@ int main(int argc, char **argv) {
   }
 
   /* Compute local sizes */
-  nnode = compute_local_size(g_nnode, comm_size, my_rank);
-  nedge = compute_local_size(g_nedge, comm_size, my_rank);
+  int nnode = compute_local_size(g_nnode, comm_size, my_rank);
+  int nedge = compute_local_size(g_nedge, comm_size, my_rank);
   op_printf("Number of nodes, edges on process %d = %d, %d\n", my_rank, nnode,
             nedge);
 
-  /*Allocate memory to hold local sets, mapping tables and data*/
-  pp = (int *)malloc(2 * sizeof(int) * nedge);
+  /* Allocate memory to hold local sets, mapping tables and data */
+  int *pp = (int *)malloc(2 * sizeof(int) * nedge);
 
-  A = (double *)malloc(nedge * sizeof(double));
-  r = (float *)malloc(nnode * sizeof(float));
-  u = (float *)malloc(nnode * sizeof(float));
-  du = (float *)malloc(nnode * sizeof(float));
+  double *A = (double *)malloc(nedge * 3 * sizeof(double));
+  float *r = (float *)malloc(nnode * 2 * sizeof(float));
+  float *u = (float *)malloc(nnode * 2 * sizeof(float));
+  float *du = (float *)malloc(nnode * 3 * sizeof(float));
 
-  /* scatter sets, mappings and data on sets*/
+  /* scatter sets, mappings and data on sets */
   scatter_int_array(g_pp, pp, comm_size, g_nedge, nedge, 2);
-  scatter_double_array(g_A, A, comm_size, g_nedge, nedge, 1);
-  scatter_float_array(g_r, r, comm_size, g_nnode, nnode, 1);
-  scatter_float_array(g_u, u, comm_size, g_nnode, nnode, 1);
-  scatter_float_array(g_du, du, comm_size, g_nnode, nnode, 1);
+  scatter_double_array(g_A, A, comm_size, g_nedge, nedge, 3);
+  scatter_float_array(g_r, r, comm_size, g_nnode, nnode, 2);
+  scatter_float_array(g_u, u, comm_size, g_nnode, nnode, 2);
+  scatter_float_array(g_du, du, comm_size, g_nnode, nnode, 3);
 
-  /*Freeing memory allocated to gloabal arrays on rank 0
-    after scattering to all processes*/
+  /* Freeing memory allocated to gloabal arrays on rank 0
+    after scattering to all processes */
   if (my_rank == MPI_ROOT) {
     free(g_pp);
+    free(g_p1);
+    free(g_p2);
+
     free(g_A);
     free(g_r);
     free(g_u);
@@ -282,10 +284,10 @@ int main(int argc, char **argv) {
 
   op_map ppedge = op_decl_map(edges, nodes, 2, pp, "ppedge");
 
-  op_dat p_A = op_decl_dat(edges, 1, "double", A, "p_A");
-  op_dat p_r = op_decl_dat(nodes, 1, "float", r, "p_r");
-  op_dat p_u = op_decl_dat(nodes, 1, "float", u, "p_u");
-  op_dat p_du = op_decl_dat(nodes, 1, "float", du, "p_du");
+  op_dat p_A = op_decl_dat(edges, 3, "double", A, "p_A");
+  op_dat p_r = op_decl_dat(nodes, 2, "float", r, "p_r");
+  op_dat p_u = op_decl_dat(nodes, 2, "float", u, "p_u");
+  op_dat p_du = op_decl_dat(nodes, 3, "float", du, "p_du");
 
   alpha = 2.0f;
   op_decl_const(1, "float", &alpha);
@@ -306,58 +308,45 @@ int main(int argc, char **argv) {
 
   for (int iter = 0; iter < NITER; iter++) {
     op_par_loop(res, "res", edges,
-                op_arg_dat(p_A, -1, OP_ID, 1, "double", OP_READ),
-                op_arg_dat(p_u, 1, ppedge, 1, "float", OP_READ),
-                op_arg_dat(p_du, 0, ppedge, 1, "float", OP_INC),
+                op_arg_dat(p_A, -1, OP_ID, 3, "double", OP_READ),
+                op_arg_dat(p_u, 1, ppedge, 2, "float", OP_READ),
+                op_arg_dat(p_du, 0, ppedge, 3, "float", OP_INC),
                 op_arg_gbl(&beta, 1, "float", OP_READ));
 
     u_sum = 0.0f;
     u_max = 0.0f;
     op_par_loop(update, "update", nodes,
-                op_arg_dat(p_r, -1, OP_ID, 1, "float", OP_READ),
-                op_arg_dat(p_du, -1, OP_ID, 1, "float", OP_RW),
-                op_arg_dat(p_u, -1, OP_ID, 1, "float", OP_INC),
+                op_arg_dat(p_r, -1, OP_ID, 2, "float", OP_READ),
+                op_arg_dat(p_du, -1, OP_ID, 3, "float", OP_RW),
+                op_arg_dat(p_u, -1, OP_ID, 2, "float", OP_INC),
                 op_arg_gbl(&u_sum, 1, "float", OP_INC),
                 op_arg_gbl(&u_max, 1, "float", OP_MAX));
-
-    op_printf("\n u max/rms = %f %f \n\n", u_max, sqrt(u_sum / g_nnode));
+    op_printf("\n u max/rms = %f %f \n\n", u_max, sqrt(u_sum / nnode));
   }
 
   op_timers(&cpu_t2, &wall_t2);
 
-  // get results data array
   op_fetch_data(p_u, u);
+  op_print_dat_to_txtfile(p_u, "out_grid_mpi.dat");
 
-  // output the result dat array to files
-  op_print_dat_to_txtfile(p_u, "out_grid_mpi.dat"); // ASCI
-  op_print_dat_to_binfile(p_u, "out_grid_mpi.bin"); // Binary
-
-  printf("solution on rank %d\n", my_rank);
-  for (int i = 0; i < nnode; i++) {
-    printf(" %7.4f", u[i]);
-    fflush(stdout);
-  }
-  printf("\n");
-
-  // print each mpi process's timing info for each kernel
   op_timing_output();
 
   // print total time for niter interations
   op_printf("Max total runtime = %f\n", wall_t2 - wall_t1);
 
-  // gather results from all ranks and check
-  float *ug = (float *)malloc(sizeof(float) * op_get_size(nodes));
+  float *ug = (float *)malloc(sizeof(float) * op_get_size(nodes) * 2);
   op_fetch_data_idx(p_u, ug, 0, op_get_size(nodes) - 1);
+
   int result = check_result<float>(ug, NN, TOLERANCE);
   free(ug);
 
   op_exit();
 
-  free(u);
   free(pp);
   free(A);
-  free(r);
+  free(u);
   free(du);
+  free(r);
 
   return result;
 }
