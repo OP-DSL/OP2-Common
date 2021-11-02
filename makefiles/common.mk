c0303bb230cdbf8f9b6b31f3b5c1339ff4e6c63c
Joe Kaushal <joe.kaushal@gmail.com>
2021-11-02 11:30:12 +0000

    Remove FORTRAN airfoil_plain generated files

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -29,6 +29,7 @@ OP2_BUILD_DIR ?= $(ROOT_DIR)/op2
 
 OP2_INC ?= -I$(ROOT_DIR)/op2/include
 OP2_LIB ?= -L$(OP2_BUILD_DIR)/lib
+OP2_MOD ?= -I$(OP2_BUILD_DIR)/mod
 
 OP2_LIBS_SINGLE_NODE := seq cuda openmp openmp4
 OP2_FOR_LIBS_SINGLE_NODE := $(foreach lib,$(OP2_LIBS_SINGLE_NODE),f_$(lib))
@@ -81,18 +82,28 @@ include $(MAKEFILES_DIR)/compilers/$(OP2_COMPILER).mk
 # Generate helper variables OP2_LIB_SEQ, OP2_LIB_MPI_CUDA, ...
 define OP2_LIB_template =
 OP2_LIB_$(call UPPERCASE,$(1)) := $(OP2_LIB) -lop2_$(1) $(2)
+OP2_LIB_FOR_$(call UPPERCASE,$(1)) := $(OP2_LIB) -lop2_for_$(1) $(3)
 endef
 
 OP2_LIB_EXTRA =
 OP2_LIB_EXTRA_MPI = $(PARMETIS_LIB) $(PTSCOTCH_LIB)
 
+OP2_LIB_FOR_EXTRA = $(OP2_LIB_EXTRA)
+OP2_LIB_FOR_EXTRA_MPI = $(OP2_LIB_EXTRA_MPI)
+
 ifeq ($(OP2_LIBS_WITH_HDF5),true)
   OP2_LIB_EXTRA += -lop2_hdf5 $(HDF5_LIB)
   OP2_LIB_EXTRA_MPI += -lop2_hdf5 $(HDF5_LIB)
+
+  OP2_LIB_FOR_EXTRA += -lop2_for_hdf5 $(HDF5_LIB)
+  OP2_LIB_FOR_EXTRA_MPI += -lop2_for_hdf5 $(HDF5_LIB)
 endif
 
-$(foreach lib,$(OP2_LIBS_SINGLE_NODE),$(eval $(call OP2_LIB_template,$(lib),$(OP2_LIB_EXTRA))))
-$(foreach lib,$(OP2_LIBS_MPI),$(eval $(call OP2_LIB_template,$(lib),$(OP2_LIB_EXTRA_MPI))))
+$(foreach lib,$(OP2_LIBS_SINGLE_NODE),$(eval $(call OP2_LIB_template,$(lib),\
+	$(OP2_LIB_EXTRA),$(OP2_LIB_FOR_EXTRA))))
+
+$(foreach lib,$(OP2_LIBS_MPI),$(eval $(call OP2_LIB_template,$(lib),\
+	$(OP2_LIB_EXTRA_MPI),$(OP2_LIB_FOR_EXTRA_MPI))))
 
 OP2_LIB_CUDA += $(CUDA_LIB)
 OP2_LIB_MPI_CUDA += $(CUDA_LIB)
