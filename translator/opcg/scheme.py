dacbb0cc6d6e13708a4a5d2c8fac6f50a4de88ab
Joe Kaushal <joe.kaushal@warwick.ac.uk>
2022-01-17 12:05:49 +0000

    Remove opcg make stubs

--- translator/opcg/scheme.py
+++ translator/opcg/scheme.py
@@ -23,7 +23,6 @@ class Scheme(object):
         lang: Lang,
         opt: Opt,
         loop_host_template: Path,
-        make_stub_template: Path = None,
         master_kernel_template: Path = None,
     ) -> None:
         if Scheme.find(lang, opt):
@@ -33,7 +32,6 @@ class Scheme(object):
         self.lang = lang
         self.opt = opt
         self.loop_host_template = loop_host_template
-        self.make_stub_template = make_stub_template
         self.master_kernel_template = master_kernel_template
 
     @classmethod
@@ -65,22 +63,6 @@ class Scheme(object):
         # Generate source from the template
         return template.render(app=app, opt=self.opt), extension
 
-    def genMakeStub(self, paths: List[Path]) -> str:
-        if self.make_stub_template is None:
-            exit(f"No Make stub template registered for {self}")
-
-        # Load the make stub template
-        template = env.get_template(str(self.make_stub_template))
-
-        source_paths = [path.name for path in paths]
-        object_paths = [path.with_suffix(".o") for path in paths]
-
-        return template.render(
-            source_paths=source_paths,
-            object_paths=object_paths,
-            opt=self.opt,
-        )
-
     def translateKernel(self, kernel: Kernel, app: Application) -> str:
         raise NotImplementedError(f'no kernel translator registered for the "{self}" scheme')
 
@@ -89,21 +71,20 @@ cseq = Scheme(
     cpp.lang,
     optimisation.seq,
     Path("cpp/seq/loop_host.cpp.j2"),
-    None,
     Path("cpp/seq/master_kernel.cpp.j2"),
 )
+
 comp = Scheme(
     cpp.lang,
     optimisation.omp,
     Path("cpp/omp/loop_host.cpp.j2"),
-    None,
     Path("cpp/omp/master_kernel.cpp.j2"),
 )
+
 ccuda = Scheme(
     cpp.lang,
     optimisation.cuda,
     Path("cpp/cuda_kepler/loop_host.cu.j2"),
-    None,
     Path("cpp/cuda_kepler/master_kernel.cu.j2"),
 )
 
@@ -115,25 +96,24 @@ fseq = Scheme(
     fortran.lang,
     optimisation.seq,
     Path("fortran/seq/loop_host.F90.j2"),
-    Path("fortran/seq/make_stub.make.j2"),
 )
+
 fvec = Scheme(
     fortran.lang,
     optimisation.vec,
     Path("fortran/vec/loop_host.F90.j2"),
-    Path("fortran/vec/make_stub.make.j2"),
 )
+
 fomp = Scheme(
     fortran.lang,
     optimisation.omp,
     Path("fortran/omp/loop_host.F90.j2"),
-    Path("fortran/omp/make_stub.make.j2"),
 )
+
 fcuda = Scheme(
     fortran.lang,
     optimisation.cuda,
     Path("fortran/cuda/loop_host.CUF.j2"),
-    Path("fortran/cuda/make_stub.make.j2"),
 )
 
 from fortran.translator.kernels import cuda
