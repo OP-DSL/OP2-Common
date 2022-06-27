90db3360354b25f481cd89bc5274498c6de4b9bd
Joe Kaushal <joe.kaushal@warwick.ac.uk>
2022-06-27 13:12:01 +0100

    op2-translator: Support for user functions and types in kernels for C++

--- translator/op2-translator/cpp/__init__.py
+++ translator/op2-translator/cpp/__init__.py
@@ -1,19 +1,40 @@
 import os
+from io import StringIO
 from functools import lru_cache
 from pathlib import Path
-from typing import FrozenSet, List, Optional, Set
+from typing import FrozenSet, List, Optional, Set, Tuple
 
 import clang.cindex
+import pcpp
 from dotenv import load_dotenv
 
 import cpp.parser
 import cpp.translator.program
 import op as OP
 from language import Lang
-from store import Kernel, ParseError, Program
+from store import Kernel, ParseError, Program, Location
 
 load_dotenv()
 clang.cindex.Config.set_library_file(os.getenv("LIBCLANG_PATH"))
+
+
+class Preprocessor(pcpp.Preprocessor):
+    def __init__(self, lexer=None):
+        super(Preprocessor, self).__init__(lexer)
+        self.line_directive = None
+
+    def on_comment(self, tok: str) -> bool:
+        return True
+
+    def on_error(self, file: str, line: int, msg: str) -> None:
+        loc = Location(file, line, 0)
+        raise ParseError(msg, loc)
+
+    def on_include_not_found(self, is_malformed, is_system_include, curdir, includepath) -> None:
+        if is_system_include:
+            raise pcpp.OutputDirective(pcpp.Action.IgnoreAndPassThrough)
+
+        super.on_include_not_found(is_malformed, is_system_include, curdir, includepath)
 
 
 class Cpp(Lang):
@@ -28,13 +49,33 @@ class Cpp(Lang):
 
     @lru_cache(maxsize=None)
     def parseFile(
-        self, path: Path, include_dirs: FrozenSet[Path], defines: FrozenSet[str]
-    ) -> clang.cindex.TranslationUnit:
+        self, path: Path, include_dirs: FrozenSet[Path], defines: FrozenSet[str], preprocess: bool = False
+    ) -> Tuple[clang.cindex.TranslationUnit, str]:
         args = [f"-I{dir}" for dir in include_dirs]
         args = args + [f"-D{define}" for define in defines]
 
+        source = path.read_text()
+        if preprocess:
+            preprocessor = Preprocessor()
+
+            for dir in include_dirs:
+                preprocessor.add_path(str(dir.resolve()))
+
+            for define in defines:
+                preprocessor.define(str)
+
+            preprocessor.parse(source, str(path.resolve()))
+            source_io = StringIO()
+            preprocessor.write(source_io)
+
+            source_io.seek(0)
+            source = source_io.read()
+
         translation_unit = clang.cindex.Index.create().parse(
-            path, args=args, options=clang.cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD
+            path,
+            unsaved_files=[(path, source)],
+            args=args,
+            options=clang.cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD,
         )
 
         for diagnostic in iter(translation_unit.diagnostics):
@@ -46,13 +87,13 @@ class Cpp(Lang):
                 f"{cpp.parser.parseLocation(diagnostic)}: {diagnostic.spelling}"
             )
 
-        return translation_unit
+        return translation_unit, source
 
     def parseProgram(self, path: Path, include_dirs: Set[Path], defines: List[str]) -> Program:
-        return cpp.parser.parseProgram(self.parseFile(path, frozenset(include_dirs), frozenset(defines)), path)
+        return cpp.parser.parseProgram(self.parseFile(path, frozenset(include_dirs), frozenset(defines))[0], path)
 
     def parseKernel(self, path: Path, name: str, include_dirs: Set[Path], defines: List[str]) -> Optional[Kernel]:
-        return cpp.parser.parseKernel(self.parseFile(path, frozenset(include_dirs), frozenset(defines)), name, path)
+        return cpp.parser.parseKernel(self.parseFile(path, frozenset(include_dirs), frozenset(defines))[0], name, path)
 
     def translateProgram(self, program: Program, include_dirs: Set[Path], defines: List[str], force_soa: bool) -> str:
         return cpp.translator.program.translateProgram(program.path.read_text(), program, force_soa)
