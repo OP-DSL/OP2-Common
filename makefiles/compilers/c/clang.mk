343a279f73497b99bfc65bcfa8e7773463bc2b31
Istvan Reguly <regulyistvan@gmail.com>
2022-08-18 15:11:43 +0200

    adds support for op_arg_idx in C

--- makefiles/compilers/c/clang.mk
+++ makefiles/compilers/c/clang.mk
@@ -4,3 +4,5 @@ CONFIG_CC := clang
 CONFIG_CXX := clang++
 
 CONFIG_CXXLINK ?= -lc++
+CONFIG_CPP_HAS_OMP_OFFLOAD ?= true
+OMP_OFFLOAD_CXXFLAGS = -fopenmp=libomp -fopenmp-targets=nvptx64-nvidia-cuda -ffp-contract=fast -Xcuda-ptxas -v -Xclang -target-feature -Xclang +ptx70 
