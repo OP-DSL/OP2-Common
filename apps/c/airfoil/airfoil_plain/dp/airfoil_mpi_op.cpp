ebd6878a2c68ec47e222103d7f92aa0e700d4db8
Suneth Ekanayake <ekanayake.suneth@gmail.com>
2022-09-02 23:22:13 +0100

    Changes to airfoil to support SLOPE

--- apps/c/airfoil/airfoil_plain/dp/airfoil_mpi_op.cpp
+++ apps/c/airfoil/airfoil_plain/dp/airfoil_mpi_op.cpp
@@ -556,14 +556,13 @@ int main(int argc, char **argv) {
 
   for (int i = 0; i < comm_size; i++) {
     if (i == my_rank) {
-      // insp_print (insp, LOW);
+      insp_print (insp, LOW);
       // generate_vtk (insp, HIGH, vertices, mesh->coords, DIM2, rank);
     }
     MPI_Barrier(MPI_COMM_WORLD);
   }
 
   int nColors = exec_num_colors (exec);
-  printf("running nColors=%d\n", nColors);
   #endif
 
   op_timers(&cpu_t1, &wall_t1);
@@ -638,8 +637,6 @@ int main(int argc, char **argv) {
                        ((double*)(p_res->data)) + lbe2c_2[k + 0]*4,
                        ((int*)(p_bound->data)) + iterations_2[k]);
           }
-          // continue;
-          // rms = 0.0;
           // loop update
           iterations_list& iterations_3 = tile_get_iterations (tile, 3);
           tileLoopSize = tile_loop_size (tile, 3);
@@ -652,8 +649,6 @@ int main(int argc, char **argv) {
           }          
         }
       }
-
-      op_print_dat_to_txtfile(p_q, "before.dat"); 
 
       op_mpi_set_dirtybit(6, args0);
       op_mpi_set_dirtybit(8, args1);
@@ -669,7 +664,6 @@ int main(int argc, char **argv) {
       op_mpi_wait_all_chained(8, args1, 1);
       op_mpi_wait_all_chained(6, args2, 1);
       op_mpi_wait_all_chained(4, args3, 1);
-      op_print_dat_to_txtfile(p_q, "after.dat"); 
 
       for (int i = 0; i < nColors; i++) {
         
@@ -708,11 +702,6 @@ int main(int argc, char **argv) {
           tileLoopSize = tile_loop_size (tile, 1);
 
           for (int k = 0; k < tileLoopSize; k++) {
-            if(my_rank == 0){
-              // printf("k=%d loopsize=%d x %d %d %d %d adt %d %d %d %d\n",k, tileLoopSize, 
-              // le2n_1[k*2 + 0], le2n_1[k*2 + 1], le2c_1[k*2 + 0], le2c_1[k*2 + 1],
-              // le2c_1[k*2 + 0], le2c_1[k*2 + 1], le2c_1[k*2 + 0], le2c_1[k*2 + 1]);
-            }
             res_calc (((double*)(p_x->data)) + le2n_1[k*2 + 0]*2,
                       ((double*)(p_x->data)) + le2n_1[k*2 + 1]*2,
                       ((double*)(p_q->data)) + le2c_1[k*2 + 0]*4,
@@ -738,7 +727,6 @@ int main(int argc, char **argv) {
                        ((double*)(p_res->data)) + lbe2c_2[k + 0]*4,
                        ((int*)(p_bound->data)) + iterations_2[k]);
           }
-          // continue;
           // loop update
           iterations_list& iterations_3 = tile_get_iterations (tile, 3);
           tileLoopSize = tile_loop_size (tile, 3);
@@ -751,42 +739,6 @@ int main(int argc, char **argv) {
           }
         }
       } 
-     
-       //    calculate area/timstep
-      // op_par_loop_adt_calc("adt_calc",cells,
-      //             op_arg_dat(p_x,0,pcell,2,"double",OP_READ),
-      //             op_arg_dat(p_x,1,pcell,2,"double",OP_READ),
-      //             op_arg_dat(p_x,2,pcell,2,"double",OP_READ),
-      //             op_arg_dat(p_x,3,pcell,2,"double",OP_READ),
-      //             op_arg_dat(p_q,-1,OP_ID,4,"double",OP_READ),
-      //             op_arg_dat(p_adt,-1,OP_ID,1,"double",OP_WRITE));
-
-      //    calculate flux residual
-      // op_par_loop_res_calc("res_calc",edges,
-      //             op_arg_dat(p_x,0,pedge,2,"double",OP_READ),
-      //             op_arg_dat(p_x,1,pedge,2,"double",OP_READ),
-      //             op_arg_dat(p_q,0,pecell,4,"double",OP_READ),
-      //             op_arg_dat(p_q,1,pecell,4,"double",OP_READ),
-      //             op_arg_dat(p_adt,0,pecell,1,"double",OP_READ),
-      //             op_arg_dat(p_adt,1,pecell,1,"double",OP_READ),
-      //             op_arg_dat(p_res,0,pecell,4,"double",OP_INC),
-      //             op_arg_dat(p_res,1,pecell,4,"double",OP_INC));
-
-      // op_par_loop_bres_calc("bres_calc",bedges,
-      //             op_arg_dat(p_x,0,pbedge,2,"double",OP_READ),
-      //             op_arg_dat(p_x,1,pbedge,2,"double",OP_READ),
-      //             op_arg_dat(p_q,0,pbecell,4,"double",OP_READ),
-      //             op_arg_dat(p_adt,0,pbecell,1,"double",OP_READ),
-      //             op_arg_dat(p_res,0,pbecell,4,"double",OP_INC),
-      //             op_arg_dat(p_bound,-1,OP_ID,1,"int",OP_READ));
-
-      //    update flow field
-      // op_par_loop_update("update",cells,
-      //             op_arg_dat(p_qold,-1,OP_ID,4,"double",OP_READ),
-      //             op_arg_dat(p_q,-1,OP_ID,4,"double",OP_WRITE),
-      //             op_arg_dat(p_res,-1,OP_ID,4,"double",OP_RW),
-      //             op_arg_dat(p_adt,-1,OP_ID,1,"double",OP_READ)); 
-
     }
       #else
 
