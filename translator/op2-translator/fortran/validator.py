90e610fd34b84ed63d778c41080407bda292b23c
Joe Kaushal <joe.kaushal@warwick.ac.uk>
2022-12-07 13:28:31 +0000

    op2-translator: Improve Fortran access type verification

--- translator/op2-translator/fortran/validator.py
+++ translator/op2-translator/fortran/validator.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Any
 
 import fparser.two.Fortran2003 as f2003
 import fparser.two.utils as fpu
@@ -35,14 +35,26 @@ def validateLoop(loop: OP.Loop, program: Program, app: Application) -> None:
         if arg.access_type != OP.AccessType.READ:
             continue
 
-        written = paramIsWritten(idx, kernel_entities[0], entities)
+        written, in_subcall = paramIsWritten(idx, kernel_entities[0], entities)
+        param_name = kernel_entities[0].parameters[idx]
 
         if written:
-            param_name = kernel_entities[0].parameters[idx]
-            print(f"Warning: arg {idx} ({param_name}) of {loop.kernel} marked OP_READ but was written")
+            msg = f"Warning: arg {idx + 1} ({param_name}) of {loop.kernel} marked OP_READ but was written"
+            if in_subcall:
+                msg = msg + " (in called subroutine)"
 
+            print(msg)
 
-def paramIsWritten(param_idx: int, func: Function, funcs: List[Function]) -> bool:
+def isRef(node: Any, name: str) -> bool: 
+    if isinstance(node, f2003.Name) and node.string == name:
+        return True
+
+    if isinstance(node, f2003.Part_Ref) and node.items[0].string == name:
+        return True
+
+    return False
+
+def paramIsWritten(param_idx: int, func: Function, funcs: List[Function]) -> (bool, bool):
     assert isinstance(func.ast, f2003.Subroutine_Subprogram)
     param_name = func.parameters[param_idx]
 
@@ -50,13 +62,8 @@ def paramIsWritten(param_idx: int, func: Function, funcs: List[Function]) -> boo
     assert execution_part != None
 
     for node in fpu.walk(execution_part, f2003.Assignment_Stmt):
-        lhs = node.items[0]
-
-        if isinstance(lhs, f2003.Name) and lhs.string == param_name:
-            return True
-
-        if isinstance(lhs, f2003.Part_Ref) and lhs.items[0].string == param_name:
-            return True
+        if isRef(node.items[0], param_name):
+            return True, False
 
     for node in fpu.walk(execution_part, f2003.Call_Stmt):
         name_node = fpu.get_child(node, f2003.Name)
@@ -75,11 +82,10 @@ def paramIsWritten(param_idx: int, func: Function, funcs: List[Function]) -> boo
             continue
 
         for param2_idx, arg in enumerate(args.items):
-            for arg_subnode in fpu.walk(arg, f2003.Name):
-                if arg_subnode.string != param_name:
-                    continue
+            if not isRef(arg, param_name):
+                continue
 
-                if paramIsWritten(param2_idx, func2, funcs):
-                    return True
+            if paramIsWritten(param2_idx, func2, funcs)[0]:
+                return True, True
 
-    return False
+    return False, False
