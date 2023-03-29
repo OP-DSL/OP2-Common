7dac63857c72f2731207ef066c26234b3f8e4d7e
Nicholas Walker <Nicholas.Walker@warwick.ac.uk>
2023-03-29 09:22:13 +0100

    DCS source script

---
+++ dcs_build.sh
@@ -0,0 +1,7 @@
+export OP2_COMPILER=gnu
+export HDF5_PAR_INSTALL_PATH=~/local/bin/hdf5
+export PARMETIS_INSTALL_PATH=~/local/
+export GPI_INSTALL_PATH=~/local/
+
+module purge
+module load cs402-mpi
