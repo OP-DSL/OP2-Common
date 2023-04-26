2ed872d37e638e79a05af4943f3af1f81b52f301
Toby Flynn <toby_flynn@btinternet.com>
2023-04-26 23:55:02 +0100

    Fixes seg fault when a nullptr is passed to op_decl_dat with SoA

--- op2/src/cuda/op_cuda_decl.cpp
+++ op2/src/cuda/op_cuda_decl.cpp
@@ -132,7 +132,7 @@ op_dat op_decl_dat_char(op_set set, int dim, char const *type, int size,
       for (int j = 0; j < set_size; j++) {
         for (int c = 0; c < element_size; c++) {
           temp_data[element_size * i * set_size + element_size * j + c] =
-              data[dat->size * j + element_size * i + c];
+              dat->data[dat->size * j + element_size * i + c];
         }
       }
     }
