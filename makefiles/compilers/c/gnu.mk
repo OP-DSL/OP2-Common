9c18c8e88122ea2b61b774d719ee3e62ba86a4f4
not populated <istvan.reguly@warwick.ac.uk>
2024-03-17 13:34:21 +0000

    Archer2 GNU issues

--- makefiles/compilers/c/gnu.mk
+++ makefiles/compilers/c/gnu.mk
@@ -5,7 +5,7 @@ CONFIG_CXX := g++
 BASE_CXXFLAGS := -MMD -MP -Wall -Wextra -pedantic
 
 ifndef DEBUG
-  BASE_CXXFLAGS += -O3
+  BASE_CXXFLAGS += -O2 -g
 
   ifeq ($(TARGET_HOST),true)
     BASE_CXXFLAGS += -march=native
