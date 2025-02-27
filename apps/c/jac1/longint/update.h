1a98114292d54edf707c79e0487b3d29d9aa6059
Istvan Reguly <regulyistvan@gmail.com>
2025-02-27 21:35:05 +0100

    distributed jac app with longs, further fixes in mpi_part_core.

---
+++ apps/c/jac1/longint/update.h
@@ -0,0 +1,7 @@
+inline void update(const double *r, double *du, double *u, int *index, double *u_sum,
+                   double *u_max) {
+  *u += *du + alpha * (*r);
+  *du = 0.0f;
+  *u_sum += (*u) * (*u);
+  *u_max = maxfun(*u_max, *u);
+}
