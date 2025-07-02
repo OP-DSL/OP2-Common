1d34a210a02e650f5c39c852be86bc81da29b497
Joe Kaushal <joe.kaushal@warwick.ac.uk>
2025-07-02 10:35:45 +0100

    mpi: Various unified exchanges tweaks

--- op2/include/op_mpi_cuda_unified_kernels.h
+++ op2/include/op_mpi_cuda_unified_kernels.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include <op_mpi_unified_exchanges.h>
+
 #include <tuple>
+#include <map>
+#include <vector>
 
 namespace op::mpi::unified {
 
 std::tuple<void *, void *> alloc_exchange_buffers(size_t gather_size, size_t scatter_size);
 
-void start_gather(const GatherSpec &gather);
-void start_scatter(const ScatterSpec &scatter);
+void initiate_gathers(const std::map<int, std::vector<GatherSpec>> &gathers_for_neighbour);
+void initiate_scatters(const std::map<int, std::vector<ScatterSpec>> &scatters_for_neighbour);
 
-void record_gathers();
 void wait_gathers();
 
 }
--- op2/src/mpi/op_mpi_cuda_unified_kernels.cu
+++ op2/src/mpi/op_mpi_cuda_unified_kernels.cu
@@ -22,6 +22,12 @@ size_t scatter_buf_size = 0;
 gpuEvent_t gather_event;
 bool gather_event_initialised = false;
 
+GatherSpec *gathers_d;
+size_t gathers_size = 0;
+
+ScatterSpec *scatters_d;
+size_t scatters_size = 0;
+
 static void ensure_capacity(void **buffer, size_t *size, size_t capacity) {
     if (capacity <= *size) {
         return;
@@ -43,61 +49,58 @@ std::tuple<void *, void *> alloc_exchange_buffers(size_t gather_size, size_t sca
     return {gather_buf, scatter_buf};
 }
 
-template<typename T>
-__global__ void gather_kernel(GatherSpec gather) {
+__global__ void gather_kernel(GatherSpec *gathers, int num_gathers) {
     int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-    if (thread_id >= gather.size) {
-        return;
+
+    int gather_index = 0;
+    int accumulated_gather_sizes = 0;
+
+    while (gather_index < num_gathers) {
+        if (thread_id < accumulated_gather_sizes + gathers[gather_index].size) break;
+
+        accumulated_gather_sizes += gathers[gather_index].size;
+        ++gather_index;
     }
 
-    auto set_elem = gather.list[thread_id];
-    for (int i = 0; i < gather.dat.dim; ++i) {
-        ((T *) gather.target)[thread_id * gather.dat.dim + i] = gather.dat.get<T>(set_elem, i);
-    }
-}
+    if (gather_index == num_gathers) return;
 
-void start_gather(const GatherSpec &gather) {
-    if (gather.size == 0) return;
-    size_t num_blocks = (gather.size + (BLOCK_SIZE - 1)) / BLOCK_SIZE;
+    auto& gather = gathers[gather_index];
+    auto index = thread_id - accumulated_gather_sizes;
 
-    switch (gather.dat.elem_size) {
-        case 4: gather_kernel<std::uint32_t><<<num_blocks, BLOCK_SIZE>>>(gather); break;
-        case 8: gather_kernel<std::uint64_t><<<num_blocks, BLOCK_SIZE>>>(gather); break;
-
-        default:
-            std::printf("Error: unexpected gather elem size: %d\n", gather.dat.elem_size);
-            exit(1);
-    }
-}
-
-template<typename T>
-__global__ void scatter_kernel(ScatterSpec scatter) {
-    int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-    if (thread_id >= scatter.size) {
-        return;
-    }
-
-    auto set_elem = scatter.is_indirect() ? scatter.list[thread_id] : scatter.offset + thread_id;
-    for (int i = 0; i < scatter.dat.dim; ++i) {
-        scatter.dat.get<T>(set_elem, i) = ((T *) scatter.source)[thread_id * scatter.dat.dim + i];
+    auto set_elem = gather.list[index];
+    if (gather.dat.elem_size == 4) {
+        for (int i = 0; i < gather.dat.dim; ++i) {
+            ((std::uint32_t *) gather.target)[index * gather.dat.dim + i] =
+                gather.dat.get<std::uint32_t>(set_elem, i);
+        }
+    } else {
+        for (int i = 0; i < gather.dat.dim; ++i) {
+            ((std::uint64_t *) gather.target)[index * gather.dat.dim + i] =
+                gather.dat.get<std::uint64_t>(set_elem, i);
+        }
     }
 }
 
-void start_scatter(const ScatterSpec &scatter) {
-    if (scatter.size == 0) return;
-    size_t num_blocks = (scatter.size + (BLOCK_SIZE - 1)) / BLOCK_SIZE;
+void initiate_gathers(const std::map<int, std::vector<GatherSpec>> &gathers_for_neighbour) {
+    if (gathers_for_neighbour.size() == 0) return;
 
-    switch (scatter.dat.elem_size) {
-        case 4: scatter_kernel<std::uint32_t><<<num_blocks, BLOCK_SIZE>>>(scatter); break;
-        case 8: scatter_kernel<std::uint64_t><<<num_blocks, BLOCK_SIZE>>>(scatter); break;
+    size_t total_gather_size = 0;
+    std::vector<GatherSpec> gathers;
 
-        default:
-            std::printf("Error: unexpected scatter elem size: %d\n", scatter.dat.elem_size);
-            exit(1);
+    for (auto &[neighbour, gathers_batch] : gathers_for_neighbour) {
+        for (auto& gather : gathers_batch) {
+            gathers.push_back(gather);
+            total_gather_size += gather.size;
+        }
     }
-}
 
-void record_gathers() {
+    ensure_capacity((void **) &gathers_d, &gathers_size, sizeof(GatherSpec) * gathers.size());
+    cutilSafeCall(gpuMemcpyAsync((void *) gathers_d, (void *) gathers.data(),
+                                 sizeof(GatherSpec) * gathers.size(), gpuMemcpyHostToDevice));
+
+    size_t num_blocks = (total_gather_size + (BLOCK_SIZE - 1)) / BLOCK_SIZE;
+    gather_kernel<<<num_blocks, BLOCK_SIZE>>>(gathers_d, gathers.size());
+
     if (!gather_event_initialised) {
         cutilSafeCall(gpuEventCreateWithFlags(&gather_event, gpuEventDisableTiming));
     }
@@ -107,6 +110,59 @@ void record_gathers() {
 
 void wait_gathers() {
     cutilSafeCall(gpuEventSynchronize(gather_event));
+}
+
+__global__ void scatter_kernel(ScatterSpec *scatters, int num_scatters) {
+    int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+
+    int scatter_index = 0;
+    int accumulated_scatter_sizes = 0;
+
+    while (scatter_index < num_scatters) {
+        if (thread_id < accumulated_scatter_sizes + scatters[scatter_index].size) break;
+
+        accumulated_scatter_sizes += scatters[scatter_index].size;
+        ++scatter_index;
+    }
+
+    if (scatter_index == num_scatters) return;
+
+    auto& scatter = scatters[scatter_index];
+    auto index = thread_id - accumulated_scatter_sizes;
+
+    auto set_elem = scatter.is_indirect() ? scatter.list[index] : scatter.offset + index;
+    if (scatter.dat.elem_size == 4) {
+        for (int i = 0; i < scatter.dat.dim; ++i) {
+            scatter.dat.get<std::uint32_t>(set_elem, i) =
+                ((std::uint32_t *) scatter.source)[index * scatter.dat.dim + i];
+        }
+    } else {
+        for (int i = 0; i < scatter.dat.dim; ++i) {
+            scatter.dat.get<std::uint64_t>(set_elem, i) =
+                ((std::uint64_t *) scatter.source)[index * scatter.dat.dim + i];
+        }
+    }
+}
+
+void initiate_scatters(const std::map<int, std::vector<ScatterSpec>> &scatters_for_neighbour) {
+    if (scatters_for_neighbour.size() == 0) return;
+
+    size_t total_scatter_size = 0;
+    std::vector<ScatterSpec> scatters;
+
+    for (auto &[neighbour, scatters_batch] : scatters_for_neighbour) {
+        for (auto& scatter : scatters_batch) {
+            scatters.push_back(scatter);
+            total_scatter_size += scatter.size;
+        }
+    }
+
+    ensure_capacity((void **) &scatters_d, &scatters_size, sizeof(ScatterSpec) * scatters.size());
+    cutilSafeCall(gpuMemcpyAsync((void *) scatters_d, (void *) scatters.data(),
+                                 sizeof(ScatterSpec) * scatters.size(), gpuMemcpyHostToDevice, op2_grp_secondary));
+
+    size_t num_blocks = (total_scatter_size + (BLOCK_SIZE - 1)) / BLOCK_SIZE;
+    scatter_kernel<<<num_blocks, BLOCK_SIZE, 0, op2_grp_secondary>>>(scatters_d, scatters.size());
 }
 
 }
