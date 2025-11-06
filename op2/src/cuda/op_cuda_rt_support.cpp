bcc7fdb558cf5eb37038413408e669849f06b2f1
Istvan Reguly <regulyistvan@gmail.com>
2025-11-06 17:23:05 +0100

    Fixing striding issues in CUDA backend and codegen

--- op2/src/cuda/op_cuda_rt_support.cpp
+++ op2/src/cuda/op_cuda_rt_support.cpp
@@ -281,7 +281,7 @@ void op_cuda_get_data(op_dat dat) {
   else
     return;
   // transpose data
-  size_t set_size = dat->set->size + dat->set->exec_size + dat->set->nonexec_size;
+  size_t set_size = round32(dat->set->size + dat->set->exec_size + dat->set->nonexec_size);
   if (strstr(dat->type, ":soa") != NULL || (OP_auto_soa && dat->dim > 1)) {
     char *temp_data = (char *)malloc(dat->size * set_size * sizeof(char));
     cutilSafeCall(gpuMemcpy(temp_data, dat->data_d, dat->size * set_size,
@@ -347,19 +347,19 @@ void cutilDeviceInit(int argc, char **argv) {
 void op_upload_dat(op_dat dat) {
   if (!OP_hybrid_gpu)
     return;
-  size_t set_size = dat->set->size + dat->set->exec_size + dat->set->nonexec_size;
+  size_t set_size = round32(dat->set->size + dat->set->exec_size + dat->set->nonexec_size);
   if (strstr(dat->type, ":soa") != NULL || (OP_auto_soa && dat->dim > 1)) {
-    char *temp_data = (char *)malloc(dat->size * round32(set_size) * sizeof(char));
+    char *temp_data = (char *)malloc(dat->size * set_size * sizeof(char));
     int element_size = dat->size / dat->dim;
     for (int i = 0; i < dat->dim; i++) {
       for (int j = 0; j < set_size; j++) {
         for (int c = 0; c < element_size; c++) {
-          temp_data[element_size * i * round32(set_size) + element_size * j + c] =
+          temp_data[element_size * i * set_size + element_size * j + c] =
               dat->data[dat->size * j + element_size * i + c];
         }
       }
     }
-    cutilSafeCall(gpuMemcpy(dat->data_d, temp_data, round32(set_size) * dat->size,
+    cutilSafeCall(gpuMemcpy(dat->data_d, temp_data, set_size * dat->size,
                              gpuMemcpyHostToDevice));
     free(temp_data);
   } else {
@@ -371,17 +371,17 @@ void op_upload_dat(op_dat dat) {
 void op_download_dat(op_dat dat) {
   if (!OP_hybrid_gpu)
     return;
-  size_t set_size = dat->set->size + dat->set->exec_size + dat->set->nonexec_size;
+  size_t set_size = round32(dat->set->size + dat->set->exec_size + dat->set->nonexec_size);
   if (strstr(dat->type, ":soa") != NULL || (OP_auto_soa && dat->dim > 1)) {
-    char *temp_data = (char *)malloc(dat->size * round32(set_size) * sizeof(char));
-    cutilSafeCall(gpuMemcpy(temp_data, dat->data_d, round32(set_size) * dat->size,
+    char *temp_data = (char *)malloc(dat->size * set_size * sizeof(char));
+    cutilSafeCall(gpuMemcpy(temp_data, dat->data_d, set_size * dat->size,
                              gpuMemcpyDeviceToHost));
     int element_size = dat->size / dat->dim;
     for (int i = 0; i < dat->dim; i++) {
       for (int j = 0; j < set_size; j++) {
         for (int c = 0; c < element_size; c++) {
           dat->data[dat->size * j + element_size * i + c] =
-              temp_data[element_size * i * round32(set_size) + element_size * j + c];
+              temp_data[element_size * i * set_size + element_size * j + c];
         }
       }
     }
