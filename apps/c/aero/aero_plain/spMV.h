d45b8b4b4df2f86fda256363fd0e6df5d84afbb0
ZamanLantra <zaman.lantra@warwick.ac.uk>
2026-02-02 12:14:46 +0000

    Remove short-hand op_arg_dat expansions from aero

--- apps/c/aero/aero_plain/spMV.h
+++ apps/c/aero/aero_plain/spMV.h
@@ -1,4 +1,5 @@
-inline void spMV(double **v, const double *K, const double **p) {
+inline void spMV(double *v0, double *v1, double *v2, double *v3, const double *K, 
+                 const double *p0, const double *p1, const double *p2, const double *p3) {
   //     double localsum = 0;
   //  for (int j=0; j<4; j++) {
   //         localsum = 0;
@@ -18,20 +19,20 @@ inline void spMV(double **v, const double *K, const double **p) {
   //         }
   //     }
   // }
-  v[0][0] += K[0] * p[0][0];
-  v[0][0] += K[1] * p[1][0];
-  v[1][0] += K[1] * p[0][0];
-  v[0][0] += K[2] * p[2][0];
-  v[2][0] += K[2] * p[0][0];
-  v[0][0] += K[3] * p[3][0];
-  v[3][0] += K[3] * p[0][0];
-  v[1][0] += K[4 + 1] * p[1][0];
-  v[1][0] += K[4 + 2] * p[2][0];
-  v[2][0] += K[4 + 2] * p[1][0];
-  v[1][0] += K[4 + 3] * p[3][0];
-  v[3][0] += K[4 + 3] * p[1][0];
-  v[2][0] += K[8 + 2] * p[2][0];
-  v[2][0] += K[8 + 3] * p[3][0];
-  v[3][0] += K[8 + 3] * p[2][0];
-  v[3][0] += K[15] * p[3][0];
+  v0[0] += K[0] * p0[0];
+  v0[0] += K[1] * p1[0];
+  v1[0] += K[1] * p0[0];
+  v0[0] += K[2] * p2[0];
+  v2[0] += K[2] * p0[0];
+  v0[0] += K[3] * p3[0];
+  v3[0] += K[3] * p0[0];
+  v1[0] += K[4 + 1] * p1[0];
+  v1[0] += K[4 + 2] * p2[0];
+  v2[0] += K[4 + 2] * p1[0];
+  v1[0] += K[4 + 3] * p3[0];
+  v3[0] += K[4 + 3] * p1[0];
+  v2[0] += K[8 + 2] * p2[0];
+  v2[0] += K[8 + 3] * p3[0];
+  v3[0] += K[8 + 3] * p2[0];
+  v3[0] += K[15] * p3[0];
 }
