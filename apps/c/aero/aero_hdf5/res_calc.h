09f364b5c26ef48f74f31bc01d530dddd9be9ab4
ZamanLantra <zaman.lantra@warwick.ac.uk>
2026-02-02 12:14:46 +0000

    Remove short-hand op_arg_dat expansions from aero

--- apps/c/aero/aero_hdf5/res_calc.h
+++ apps/c/aero/aero_hdf5/res_calc.h
@@ -1,5 +1,11 @@
-inline void res_calc(const double **x, const double **phim, double *K,
-                     /*double *Kt,*/ double **res, double **none) {
+inline void res_calc(const double *x0, const double *x1, const double *x2, const double *x3, 
+                     const double *phim0, const double *phim1, const double *phim2, const double *phim3, 
+                     double *K, /*double *Kt,*/ double *res0, double *res1, double *res2, double *res3) {
+  double x[4][2], phim[4];
+  x[0][0] = x0[0]; x[1][0] = x1[0]; x[2][0] = x2[0]; x[3][0] = x3[0]; 
+  x[0][1] = x0[1]; x[1][1] = x1[1]; x[2][1] = x2[1]; x[3][1] = x3[1]; 
+  phim[0] = phim0[0]; phim[1] = phim1[0]; phim[2] = phim2[0]; phim[3] = phim3[0]; 
+
   for (int j = 0; j < 4; j++) {
     for (int k = 0; k < 4; k++) {
       K[j * 4 + k] = 0;
@@ -45,17 +51,19 @@ inline void res_calc(const double **x, const double **phim, double *K,
 
     double u[2] = {0.0, 0.0};
     for (int j = 0; j < 4; j++) {
-      u[0] += N_x[j] * phim[j][0];
-      u[1] += N_x[4 + j] * phim[j][0];
+      u[0] += N_x[j] * phim[j];
+      u[1] += N_x[4 + j] * phim[j];
     }
 
     double Dk = 1.0 + 0.5 * gm1 * (m2 - (u[0] * u[0] + u[1] * u[1]));
     double rho = pow(Dk, gm1i); // wow this might be problematic -> go to log?
     double rc2 = rho / Dk;
 
-    for (int j = 0; j < 4; j++) {
-      res[j][0] += wt1 * rho * (u[0] * N_x[j] + u[1] * N_x[4 + j]);
-    }
+    res0[0] += wt1 * rho * (u[0] * N_x[0] + u[1] * N_x[4 + 0]);
+    res1[0] += wt1 * rho * (u[0] * N_x[1] + u[1] * N_x[4 + 1]);
+    res2[0] += wt1 * rho * (u[0] * N_x[2] + u[1] * N_x[4 + 2]);
+    res3[0] += wt1 * rho * (u[0] * N_x[3] + u[1] * N_x[4 + 3]);
+
     for (int j = 0; j < 4; j++) {
       for (int k = 0; k < 4; k++) {
         K[j * 4 + k] +=
